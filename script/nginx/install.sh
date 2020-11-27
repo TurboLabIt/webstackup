5215e5da8391e57808696b30ae6d88308c0b643e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2020-11-27 23:31:57 +0100

    Create install.sh

---
+++ script/nginx/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! [ $(id -u) = 0 ]; then
+
+    echo "This script must run as ROOT"
+    exit
+fi
+  
+## Add Nginx key and repo
+apt install curl gnupg2 ca-certificates lsb-release unzip nano -y
+curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
+echo "deb [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ $(lsb_release -sc) nginx" > /etc/apt/sources.list.d/nginx.list
+echo "deb-src [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ $(lsb_release -sc) nginx" >> /etc/apt/sources.list.d/nginx.list
+	
+## Pinning the repo
+NGINX_PINNING_FILE=/etc/apt/preferences.d/99-nginx-webstackup
+echo "Package: nginx" > $NGINX_PINNING_FILE
+echo -n "Pin: release a=" >> $NGINX_PINNING_FILE
+echo "$(lsb_release -sc)" >> $NGINX_PINNING_FILE
+echo "Pin-Priority: -900" >> $NGINX_PINNING_FILE
+	
+## Install Nginx
+apt update -qq && apt install nginx -y
