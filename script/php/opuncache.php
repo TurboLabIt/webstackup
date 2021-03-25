729d8927ca64a0785da688f94ac916415b63308f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-03-26 00:07:39 +0100

    Delete opuncache.php

--- script/php/opuncache.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<pre>";
-echo "opcache" . PHP_EOL;
-echo "=======" . PHP_EOL;
-echo opcache_reset()  ? "OPcache flushed OK" : "ALERT! OPcache did not flush!";
-echo PHP_EOL;
-echo "</pre>";
