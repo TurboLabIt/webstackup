672a5cb5c05bdf3eb42e0cc5175fe3fa1cebf920
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-06-29 23:13:38 +0200

    Update mysqltuner.sh

--- script/mysql/mysqltuner.sh
+++ script/mysql/mysqltuner.sh
@@ -6,6 +6,6 @@ printHeader "MySQL Tuner"
 rootCheck
 
 source /etc/turbolab.it/mysql.conf
-mysqltuner --host 127.0.0.1 --user $MYSQL_USER --pass $MYSQL_PASSWORD
+mysqltuner --user $MYSQL_USER --pass $MYSQL_PASSWORD
 
 printMessage "MySQL Tuner is done"
