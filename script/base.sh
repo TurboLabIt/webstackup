6cc32183c957319fbe34a0fdd7ea4ecdc528cc58
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-07-19 21:42:30 +0200

    Update base.sh

--- script/base.sh
+++ script/base.sh
@@ -168,3 +168,25 @@ fi
 
 INSTALLED_RAM=$(awk '/MemFree/ { printf "%.3f \n", $2/1024/1024 }' /proc/meminfo)
 INSTALLED_RAM="${INSTALLED_RAM//.}"
+
+GIT_BRANCH=$(git -C $PROJECT_DIR branch | grep \* | cut -d ' ' -f2-)
+
+if [ -f "${PROJECT_DIR}env" ]; then
+
+  APP_ENV=$(head -n 1 ${PROJECT_DIR}env)
+
+elif [ "$GIT_BRANCH" = "master" ]; then
+
+  APP_ENV=prod
+  
+elif [ "$GIT_BRANCH" = "staging" ]; then
+
+  APP_ENV=staging
+  
+elif [ "$GIT_BRANCH" = "dev" ]; then
+
+  APP_ENV=dev
+
+fi
+
+
