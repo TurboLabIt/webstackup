e988fa96ba96c84b9e03d622c293b33d78ae040d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-10-07 12:51:16 +0200

    Update letsencrypt-create-hooks.sh

--- script/https/letsencrypt-create-hooks.sh
+++ script/https/letsencrypt-create-hooks.sh
@@ -2,11 +2,20 @@
 
 ## Let's Encrypt post-renewal hook
 # https://certbot.eff.org/docs/using.html?highlight=hook#renewing-certificates
+#
+# Usage:
+## Let's Encrypt post-renewal hook
+# source <(curl -Ls https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/https/letsencrypt-create-hooks.sh)
+
 if [ ! -d "/etc/letsencrypt/renewal-hooks/deploy/" ]; then
 
-  echo "Deploying Let's Encrypt post-renewal hook..."
+  echo "🪝 Deploying Let's Encrypt post-renewal hook..."
   sudo curl -Lo /etc/letsencrypt/renewal-hooks/deploy/nginx_restart https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/nginx/restart.sh
   chown root:root /etc/letsencrypt/renewal-hooks/deploy/nginx_restart
   sudo chmod u=rwx,go=rx /etc/letsencrypt/renewal-hooks/deploy/nginx_restart
   sudo certbot renew --force-renewal
+  
+else
+
+  echo "Let's Encrypt post-renewal hook skipped"
 fi
