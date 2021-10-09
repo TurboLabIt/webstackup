f0a83d3bcdbcb6863f52e10e6f04b2a06e74d21f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-10-09 10:34:13 +0200

    Update letsencrypt-create-hooks.sh

--- script/https/letsencrypt-create-hooks.sh
+++ script/https/letsencrypt-create-hooks.sh
@@ -5,7 +5,7 @@
 #
 # Usage:
 ## Let's Encrypt post-renewal hook
-# source <(curl -Ls https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/https/letsencrypt-create-hooks.sh?$(date +%s))
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/https/letsencrypt-create-hooks.sh?$(date +%s) | sudo bash
 
 if [ -d "/etc/letsencrypt/renewal-hooks/deploy/" ]; then
 
