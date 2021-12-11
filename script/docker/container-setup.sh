a78060790b36853dafc0d7f38b72602af05e10d8
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-11 23:38:55 +0100

    Update container-setup.sh

--- script/docker/container-setup.sh
+++ script/docker/container-setup.sh
@@ -8,6 +8,6 @@ if ! [ $(id -u) = 0 ]; then
     exit
 fi
 
-apt install sudo wget nano openssh-server -y
+apt install apt-utils sudo wget nano openssh-server -y
 apt dist-upgrade -y
 apt autoremove -y
