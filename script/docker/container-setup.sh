af6f8c225b8dcb97d8fb79df8243d8f2c1dace39
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-11 23:14:42 +0100

    Update container-setup.sh

--- script/docker/container-setup.sh
+++ script/docker/container-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ### AUTOMATIC DOCKER CONTAINER SETUP BY WEBSTACK.UP
-# apt update && apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/docker/constainer-setup.sh?$(date +%s) | bash
+# apt update && apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/docker/container-setup.sh?$(date +%s) | bash
 
 if ! [ $(id -u) = 0 ]; then
 
