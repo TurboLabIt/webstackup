a0fa51ade63f47fb8a943c4f61cd68ef48782295
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2021-12-18 20:42:25 +0100

    Update startup.sh

--- script/docker/startup.sh
+++ script/docker/startup.sh
@@ -1,1 +1,12 @@
-aa
+#!/usr/bin/env bash
+source /usr/local/turbolab.it/webstackup/script/base.sh
+service ssh start
+service nginx start
+service ${PHP_FPM} start
+
+if [ -f /usr/local/turbolab.it/zzalias.sh ]; then
+  source /usr/local/turbolab.it/zzalias.sh
+fi
+
+echo "Here we go..."
+/usr/bin/bash
