9de169e52d5bf1c2fddf67cbe0f9b0c70ae17276
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-03-20 10:23:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,27 +12,29 @@ INSTALL_DIR=${INSTALL_DIR_PARENT}${SCRIPT_NAME}/
 ## /etc/ config directory
 mkdir -p "/etc/turbolab.it/"
 
-## Pre-requisites
-apt update
-apt install git -y
-
 ## Install/update
 echo ""
 if [ ! -d "$INSTALL_DIR" ]; then
+
+  ## Pre-requisites
+  apt update && apt install git -y
+
   echo "Installing..."
   echo "-------------"
   mkdir -p "$INSTALL_DIR_PARENT"
   cd "$INSTALL_DIR_PARENT"
   git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
+  
 else
+
   echo "Updating..."
   echo "----------"
+  
 fi
 
 ## Fetch & pull new code
 cd "$INSTALL_DIR"
 git pull --no-rebase
-
 
 ## Restore working directory
 cd $WORKING_DIR_ORIGINAL
