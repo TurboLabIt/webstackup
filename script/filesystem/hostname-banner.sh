a9cb615640393f0212b61850807efee55246fb57
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-04-11 21:44:04 +0200

    Update hostname-banner.sh

--- script/filesystem/hostname-banner.sh
+++ script/filesystem/hostname-banner.sh
@@ -3,4 +3,4 @@ if [ -z "$(command -v figlet)" ] || [ -z "$(command -v lolcat)" ]; then
   sudo apt install figlet lolcat -y -qq
 fi
 
-figlet "$(hostname)" | /usr/games/lolcat -a -d 500
+figlet "$(hostname)" | /usr/games/lolcat
