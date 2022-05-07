13be2e9403dc282d304e2461c3db905600afa2d6
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-05-07 10:11:28 +0200

    Update iptables-reset.sh

--- script/firewall/iptables-reset.sh
+++ script/firewall/iptables-reset.sh
@@ -1,16 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+### FIREWALL FACTORY RESET BY WEBSTACKUP
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/firewall/iptables-reset.sh?$(date +%s) | sudo bash
+# 
+
 echo ""
+echo -e "\e[1;46m ==================== \e[0m"
+echo -e "\e[1;46m ❤️‍🩹 FIREWALL RESET \e[0m"
+echo -e "\e[1;46m ==================== \e[0m"
 
-source "/usr/local/turbolab.it/webstackup/script/base.sh"
-printHeader "❤️‍🩹 iptables-reset"
-rootCheck
+if ! [ $(id -u) = 0 ]; then
+  echo -e "\e[1;41m This script must run as ROOT \e[0m"
+  exit
+fi
 
-printMessage "ACCEPT everything..."
+echo -e "\e[1;33m Checking UFW... \e[0m"
+if [ -z "$(command -v ufw)" ]; then
+
+  echo -e "\e[1;32m ✔ ufw is not installed \e[0m"
+  
+elif [ $(ufw status | grep -qw active) != 1 ]; then
+
+  echo -e "\e[1;33m "Disabling UFW... \e[0m"
+  ufw --force reset
+  ufw disable
+  
+else
+
+  echo -e "\e[1;32m ✔ ufw is installed, but it looks inactive \e[0m"
+
+fi
+
+echo -e "\e[1;33m ACCEPT everything... \e[0m"
 iptables -P INPUT ACCEPT
 
-printMessage "Clearing the rules..."
+echo -e "\e[1;33m Clearing the rules... \e[0m"
 iptables -F
 
-printMessage "Saving..."
+echo -e "\e[1;33m Saving... \e[0m"
 
-printMessage "iptables-reset is done"
+echo -e "\e[1;32m ✔ Firewall reset completed! \e[0m"
