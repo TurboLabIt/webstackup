c75b2f0d0ead3ba8ec2f082f82f1c92b7e84aaf4
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-06-20 09:32:25 +0200

    Update cache-clear.sh

--- script/frameworks/symfony/cache-clear.sh
+++ script/frameworks/symfony/cache-clear.sh
@@ -8,14 +8,13 @@ SCRIPT_NAME=symfony-cache-clear
 source $(dirname $(readlink -f $0))/script_begin.sh
 printHeader "🧹 cache-clear"
 
-if [ -d "${PROJECT_DIR}var/cache" ]; then
-  sudo rm -rf "/tmp/webstackup-symfony-cache-clear"
-  sudo mv "${PROJECT_DIR}var/cache" "/tmp/webstackup-symfony-cache-clear"
-  sudo rm -rf "/tmp/webstackup-symfony-cache-clear"
-fi
+sudo service nginx -t && sudo service nginx stop
+sudo service php${PHP_VER}-fpm stop
 
+sudo rm -rf "${PROJECT_DIR}var/cache"
 sudo -u "${EXPECTED_USER}" -H symfony console cache:clear
 
 sudo service php${PHP_VER}-fpm restart
+sudo service nginx -t && sudo service nginx restart
 
 source $(dirname $(readlink -f $0))/script_end.sh
