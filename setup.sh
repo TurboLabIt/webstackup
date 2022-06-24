6eca1eb9812278413c644522b5c73d36d2a1ae74
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-06-24 16:54:51 +0200

    Update setup.sh
    bash-fx based setup

--- setup.sh
+++ setup.sh
@@ -1,45 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo ""
-
-## Script name
 SCRIPT_NAME=webstackup
 
-## Install directory
-WORKING_DIR_ORIGINAL="$(pwd)"
-INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
-INSTALL_DIR=${INSTALL_DIR_PARENT}${SCRIPT_NAME}/
+## bash-fx
+if [ -z "$(command -v curl)" ]; then
+  sudo apt update && sudo apt install curl -y
+fi
+curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(date +%s) | sudo bash
+source /usr/local/turbolab.it/bash-fx/bash-fx.sh
+## bash-fx is ready
 
-## /etc/ config directory
-mkdir -p "/etc/turbolab.it/"
+sudo bash /usr/local/turbolab.it/bash-fx/setup/start.sh ${SCRIPT_NAME}
 
-## Install/update
-echo ""
-if [ ! -d "$INSTALL_DIR" ]; then
-
-  ## Pre-requisites
-  apt update && apt install git -y
-
-  echo "Installing..."
-  echo "-------------"
-  mkdir -p "$INSTALL_DIR_PARENT"
-  cd "$INSTALL_DIR_PARENT"
-  git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
-  
-else
-
-  echo "Updating..."
-  echo "----------"
-  
+## Symlink (globally-available webstackup command)
+if [ ! -f "/usr/local/bin/${SCRIPT_NAME}" ]; then
+  ln -s ${INSTALL_DIR}script/${SCRIPT_NAME}.sh /usr/local/bin/${SCRIPT_NAME}
 fi
 
-## Fetch & pull new code
-cd "$INSTALL_DIR"
-git pull --no-rebase
-
-## Restore working directory
-cd $WORKING_DIR_ORIGINAL
-
-echo ""
-echo "Setup completed!"
-echo "----------------"
-echo "See https://github.com/TurboLabIt/${SCRIPT_NAME} for the quickstart guide."
+sudo bash /usr/local/turbolab.it/bash-fx/setup/the-end.sh ${SCRIPT_NAME}
