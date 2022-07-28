bd16f9dc115b225cde19b21adf291f36a3581240
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-28 10:37:21 +0200

    Update version-variables.sh

--- script/php/version-variables.sh
+++ script/php/version-variables.sh
@@ -57,7 +57,7 @@ function wsuMage()
     fxCatastrophicError "EXPECTED_USER not set"
   fi
   
-  local CURR_DIR_BACKUP=$(cd)
+  local CURR_DIR_BACKUP=$(pwd)
 
   cd "${MAGENTO_DIR}"
   sudo -u "${EXPECTED_USER}" -H ${MAGE_CLI_EXE} $@
