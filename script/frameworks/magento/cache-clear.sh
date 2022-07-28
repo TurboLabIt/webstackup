7763831557028aabc5044d133eb2a07974cc0b71
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-28 16:26:23 +0200

    Update cache-clear.sh

--- script/frameworks/magento/cache-clear.sh
+++ script/frameworks/magento/cache-clear.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 ## Standard Magento cache-clearing routine by WEBSTACKUP
 #
-# To be used via https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/magento/cache-clear-template.sh
+# How to:
+# Copy this file to your project directory with:
+#   curl -Lo script/cache-clear.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/frameworks/magento/cache-clear-template.sh && sudo chmod u=rwx,go=rx script/cache-clear.sh
+# You should then git commit your copy
 
 SCRIPT_NAME=magento-cache-clear
 fxHeader "🧹 cache-clear"
