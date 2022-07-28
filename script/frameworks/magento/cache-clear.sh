aecb99a4e367b5f031cc7eb88310dd64580a145b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-28 17:14:00 +0200

    Update cache-clear.sh

--- script/frameworks/magento/cache-clear.sh
+++ script/frameworks/magento/cache-clear.sh
@@ -7,14 +7,41 @@
 # You should then git commit your copy
 
 SCRIPT_NAME=magento-cache-clear
-fxHeader "🧹 cache-clear"
+fxHeader "🧹 Magento cache-clear"
 
 showPHPVer
+
+if [ -z "${MAGENTO_DIR}" ] || [ ! -d "${MAGENTO_DIR}" ]; then
+  fxCatastrophicError "📁 MAGENTO_DIR not set"
+fi
+
+cd "$MAGENTO_DIR"
 
 fxTitle "Stopping services.."
 sudo nginx -t && sudo service nginx stop && sudo service ${PHP_FPM} stop
 
+sudo rm -rf \
+  "pub/static/frontend/" \
+  "pub/static/adminhtml/" \
+  "pub/static/_requirejs" \
+  "pub/static/deployed_version.txt" \
+  "var/cache/" \ 
+  "var/page_cache/" \
+  "generated/" \ 
+  "var/view_preprocessed/" \
+  "var/session/" \
+  "var/di/"
+              
+wsuMage setup:di:compile
+
+wsuMage setup:static-content:deploy --area adminhtml it_IT en_US -f
+wsuMage setup:static-content:deploy -t $@ -f
+
 wsuMage cache:flush
+
+sudo chown ${EXPECTED_USER}:www-data . -R
+sudo find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+sudo find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
 
 fxTitle "Restarting services.."
 sudo nginx -t && sudo service ${PHP_FPM} restart && sudo service nginx restart
