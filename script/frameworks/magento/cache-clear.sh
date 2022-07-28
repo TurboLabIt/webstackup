4a5fffd87e3e46f1599b54953b46fd4b2f801142
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-28 22:48:57 +0200

    Update cache-clear.sh

--- script/frameworks/magento/cache-clear.sh
+++ script/frameworks/magento/cache-clear.sh
@@ -15,35 +15,49 @@ if [ -z "${MAGENTO_DIR}" ] || [ ! -d "${MAGENTO_DIR}" ]; then
   fxCatastrophicError "📁 MAGENTO_DIR not set"
 fi
 
-cd "$MAGENTO_DIR"
-
-fxTitle "⚙️ Stopping services.."
-sudo nginx -t && sudo service nginx stop && sudo service ${PHP_FPM} stop
-
-fxTitle "🧹 Removing folders..."
-sudo rm -rf \
-  "pub/static/frontend/" \
-  "pub/static/adminhtml/" \
-  "pub/static/_requirejs" \
-  "pub/static/deployed_version.txt" \
-  "var/cache/" \
-  "var/page_cache/" \
-  "generated/" \
-  "var/view_preprocessed/" \
-  "var/session/" \
-  "var/di/"
-
-fxTitle "🧙🏗️ setup:di:compile..."
-wsuMage setup:di:compile
-
-fxTitle "🧙🏗️ static-content:deploy admin"
-wsuMage setup:static-content:deploy --area adminhtml it_IT en_US -f
-
-if [ ! -z "${MAGENTO_STATIC_CONTENT_DEPLOY}" ]; then
-  fxTitle "🧙🏗️ static-content:deploy -t ${MAGENTO_STATIC_CONTENT_DEPLOY}"
-  wsuMage setup:static-content:deploy -t ${MAGENTO_STATIC_CONTENT_DEPLOY} -f
+if [ "$1" = "fast"]; then
+  FAST_CACHE_CLEAR=1
 fi
 
+cd "$MAGENTO_DIR"
+
+if [ -z "${FAST_CACHE_CLEAR}"]; then
+
+  fxTitle "⚙️ Stopping services.."
+  sudo nginx -t && sudo service nginx stop && sudo service ${PHP_FPM} stop
+
+  fxTitle "🧹 Removing folders..."
+  sudo rm -rf \
+    "pub/static/frontend/" \
+    "pub/static/adminhtml/" \
+    "pub/static/_requirejs" \
+    "pub/static/deployed_version.txt" \
+    "var/cache/" \
+    "var/page_cache/" \
+    "generated/" \
+    "var/view_preprocessed/" \
+    "var/session/" \
+    "var/di/"
+
+  fxTitle "🧙🏗️ setup:di:compile..."
+  wsuMage setup:di:compile
+
+  fxTitle "🧙🏗️ static-content:deploy admin"
+  wsuMage setup:static-content:deploy --area adminhtml it_IT en_US -f
+
+  if [ ! -z "${MAGENTO_STATIC_CONTENT_DEPLOY}" ]; then
+    fxTitle "🧙🏗️ static-content:deploy -t ${MAGENTO_STATIC_CONTENT_DEPLOY}"
+    wsuMage setup:static-content:deploy -t ${MAGENTO_STATIC_CONTENT_DEPLOY} -f
+    
+  fi
+  
+else
+
+  fxTitle "🧙🏗️ di:compile and static-content:deploy skipped (fast mode)"
+  
+fi
+
+fxTitle "🌊 cache:flush..."
 wsuMage cache:flush
 
 fxTitle "🐧 Setting the owner..."
@@ -53,5 +67,9 @@ fxTitle "🐧 Setting permissions..."
 sudo find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
 sudo find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
 
-fxTitle "⚙️ Restarting services.."
-sudo nginx -t && sudo service ${PHP_FPM} restart && sudo service nginx restart
+if [ -z "${FAST_CACHE_CLEAR}"]; then
+
+  fxTitle "⚙️ Restarting services.."
+  sudo nginx -t && sudo service ${PHP_FPM} restart && sudo service nginx restart
+  
+fi
