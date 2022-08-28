fb03bd6c392617f3cbe0309479f893d6b0cf6b69
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-08-28 10:55:36 +0200

    Create install.sh

---
+++ script/syncthing/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+### AUTOMATIC SYNCTHING INSTALL BY WEBSTACK.UP
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/syncthing/install.sh?$(date +%s) | sudo bash
+#
+
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "🔄 Syncthing automatic installer"
+rootCheck
+
+## https://apt.syncthing.net/ ##
+################################
+
+# Add the release PGP keys:
+curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Increase preference of Syncthing's packages ("pinning")
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates syncthing -y
+
+fxEndFooter
