819d1df153f0255c0e33528e33b20e7419d45909
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-01 15:35:43 +0200

    Update mysqltuner.sh

--- script/mysql/mysqltuner.sh
+++ script/mysql/mysqltuner.sh
@@ -17,16 +17,15 @@ rootCheck
 # https://github.com/major/MySQLTuner-perl#downloadinstallation #
 #################################################################
 
-MYSQLTUNER=/usr/local/bin/mysqltuner.pl
 MYSQLTUNER_DATA_DIR=/var/lib/mysqltuner/
 mkdir ${MYSQLTUNER_DATA_DIR}
 
 fxTitle "Install/update..."
-curl -o ${MYSQLTUNER} https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl
+curl -o ${MYSQLTUNER_DATA_DIR}mysqltuner.pl https://raw.githubusercontent.com/major/MySQLTuner-perl/master/mysqltuner.pl
 curl -o ${MYSQLTUNER_DATA_DIR}basic_passwords.txt https://raw.githubusercontent.com/major/MySQLTuner-perl/master/basic_passwords.txt
 curl -o ${MYSQLTUNER_DATA_DIR}vulnerabilities.csv https://raw.githubusercontent.com/major/MySQLTuner-perl/master/vulnerabilities.csv
 
 source /etc/turbolab.it/mysql.conf
-perl ${MYSQLTUNER} --user "$MYSQL_USER" --pass "$MYSQL_PASSWORD"
+perl ${MYSQLTUNER_DATA_DIR}mysqltuner.pl --user "$MYSQL_USER" --pass "$MYSQL_PASSWORD"
 
 fxEndFooter
