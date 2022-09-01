d2a5441bee3f4353a3b127497add8c12cffa865a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-01 15:38:10 +0200

    Update mysqltuner.sh

--- script/mysql/mysqltuner.sh
+++ script/mysql/mysqltuner.sh
@@ -26,6 +26,6 @@ curl -o ${MYSQLTUNER_DATA_DIR}basic_passwords.txt https://raw.githubusercontent.
 curl -o ${MYSQLTUNER_DATA_DIR}vulnerabilities.csv https://raw.githubusercontent.com/major/MySQLTuner-perl/master/vulnerabilities.csv
 
 source /etc/turbolab.it/mysql.conf
-perl ${MYSQLTUNER_DATA_DIR}mysqltuner.pl --user "$MYSQL_USER" --pass "$MYSQL_PASSWORD"
+perl ${MYSQLTUNER_DATA_DIR}mysqltuner.pl --user "$MYSQL_USER" --pass "$MYSQL_PASSWORD" --color
 
 fxEndFooter
