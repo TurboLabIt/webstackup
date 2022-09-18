9419f0eb8d252b96706ff4d274d45a20327d61f0
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-18 17:59:03 +0200

    Update benchmark.sh

--- script/performance/benchmark.sh
+++ script/performance/benchmark.sh
@@ -15,13 +15,16 @@ fxHeader "⏲️ Benchmark"
 rootCheck
 
 fxTitle "💿 Installing...."
-apt update && apt install sysbench sysstat -y
+apt update && apt install sysbench sysstat fio -y
 
 fxTitle "CPU"
 sysbench cpu --threads=1 run
 fxMessage "Cfr i7-9750H: 1415 (events per second) | 14162 (total number of events)"
 
-fxTitle "Disk"
+fxTitle "Disk (iostat)"
 iostat -d | grep -vi loop
+
+fxTitle "Disk (fio)"
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fiotest --filename=testfio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75
 
 fxEndFooter
