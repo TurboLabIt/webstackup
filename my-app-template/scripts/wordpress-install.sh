ee4b1d20c6cfa2681e82503492350137c5e9aa11
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 00:45:23 +0200

    Update wordpress-install.sh

--- my-app-template/scripts/wordpress-install.sh
+++ my-app-template/scripts/wordpress-install.sh
@@ -7,10 +7,10 @@ source $(dirname $(readlink -f $0))/script_begin.sh
 
 source /etc/turbolab.it/mysql-my-app.conf
 
-WORDPRESS_SITE_NAME=my-app
+WORDPRESS_SITE_NAME="My App Name"
 WORDPRESS_LOCALE=it_IT
 WORDPRESS_ADMIN_USERNAME=my-name
-WORDPRESS_ADMIN_EMAIL=my-name@my-app.com
+WORDPRESS_ADMIN_EMAIL=admin@my-app.com
 WORDPRESS_MULTISITE_MODE=
 WORDPRESS_ADMIN_NEW_SLUG=my-app$(date +"%Y")
 
