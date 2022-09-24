982c0592e3f39122ed0a1678d7127525c3b08cc7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-24 18:54:57 +0200

    Update password-reset.sh

--- script/mysql/password-reset.sh
+++ script/mysql/password-reset.sh
@@ -19,16 +19,21 @@ rootCheck
 echo "Manine dimenticose edition"
 echo ""
 
-source "/usr/local/turbolab.it/webstackup/script/base.sh"
+WSU_BASE="/usr/local/turbolab.it/webstackup/script/base.sh"
+if [ -f "${WSU_BASE}" ]; then
+  source "$WSU_BASE"
+fi
+
 
 TARGET_MYSQL_USER=$1
 TARGET_MYSQL_PASSWORD=$2
 TARGET_MYSQL_USER_HOST=$3
 
+
 fxTitle "🧔 Username"
 while [ -z "$TARGET_MYSQL_USER" ]
 do
-  read -p "🤖 Provide the username: " TARGET_MYSQL_USER  < /dev/tty
+  read -p "🤖 Provide the username (leave blank for root): " TARGET_MYSQL_USER  < /dev/tty
   if [ -z "$TARGET_MYSQL_USER" ]; then
     TARGET_MYSQL_USER=root
   fi
@@ -36,11 +41,11 @@ done
 
 fxOK "OK, $TARGET_MYSQL_USER"
 
+
 fxTitle "🔑 Password"
 while [ -z "$TARGET_MYSQL_PASSWORD" ]
 do
   read -p "🤖 Provide the password (leave blank for autogeneration): " TARGET_MYSQL_PASSWORD  < /dev/tty
-
   if [ -z "$TARGET_MYSQL_PASSWORD" ]; then
     TARGET_MYSQL_PASSWORD="$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 19)"
   fi
@@ -48,21 +53,23 @@ done
 
 fxOK "OK, $TARGET_MYSQL_PASSWORD"
 
+
 fxTitle "${TARGET_MYSQL_USER}@ host"
 while [ -z "$TARGET_MYSQL_USER_HOST" ]
 do
-  read -p "🤖 Provide the host (leave blank for localhost): " TARGET_MYSQL_USER_HOST  < /dev/tty
-
+  read -p "🤖 Provide the host (leave blank for @localhost): " TARGET_MYSQL_USER_HOST  < /dev/tty
   if [ -z "$TARGET_MYSQL_USER_HOST" ]; then
-    TARGET_MYSQL_USER_HOST="localhost"
+    TARGET_MYSQL_USER_HOST=localhost
   fi
 done
 
 fxOK "OK, $TARGET_MYSQL_USER_HOST"
 
-fxTitle "New credentials preview"
+
+fxTitle "Preview"
 echo "User:  ##${TARGET_MYSQL_USER}##@##${TARGET_MYSQL_USER_HOST}##"
 echo "Pass:  ##${TARGET_MYSQL_PASSWORD}##"
+
 
 fxTitle "Creating MySQL init file..."
 MYSQL_PASSWD_RESET_FILE=/tmp/mysql_password_reset.sql
@@ -73,28 +80,41 @@ echo \
 
 fxMessage "$(cat ${MYSQL_PASSWD_RESET_FILE})"
 
+
 fxTitle "Securing the init file..."
 chown mysql:root "${MYSQL_PASSWD_RESET_FILE}"
 chmod ug=rw,o= "${MYSQL_PASSWD_RESET_FILE}"
 ls -l "${MYSQL_PASSWD_RESET_FILE}"
 
-fxTitle "Attempting to stop Nginx..."
-service nginx stop
+
+fxTitle "Checking if nginx is active and needs stopping..."
+systemctl is-active --quiet nginx
+if [ "$?" = "0" ]; then
+  NGINX_RESTART_NEEDED=1
+  fxMessage "Stopping nginx..."
+  service nginx stop
+  fxOK
+else
+  fxInfo "nginx not detected"
+fi
+
 
 fxTitle "Stopping MySQL..."
 service mysql stop
 
+
 fxTitle "Starting MySQL as application..."
-# mkdir -p /var/run/mysqld
-# chown mysql:mysql /var/run/mysqld
 sudo -u mysql -H mysqld --skip-networking --init-file="${MYSQL_PASSWD_RESET_FILE}" &
 sleep 15
+
 
 fxTitle "Removing the init file..."
 rm -f "${MYSQL_PASSWD_RESET_FILE}"
 
+
 fxTitle "Display MySQL error log..."
 tail -n 10 /var/log/mysql/error.log
+
 
 fxTitle "Stopping MySQL as application..."
 while pkill mysqld; do
@@ -102,12 +122,26 @@ while pkill mysqld; do
   sleep 15
 done
 
+
 fxTitle "Starting MySQL..."
 service mysql start
 
-fxTitle "Attempting to restart Nginx..."
-service nginx start
 
-wsuMysqlStoreCredentials "$TARGET_MYSQL_USER" "$TARGET_MYSQL_PASSWORD"
+fxTitle "Checking if nginx must be restarted..."
+if [ ! -z "$NGINX_RESTART_NEEDED" ]; then
+  fxMessage "Restarting nginx..."
+  service nginx restart
+  fxOK
+else
+  fxInfo "nginx not detected"
+fi
+
+
+if [ -f "${WSU_BASE}" ]; then
+  wsuMysqlStoreCredentials "$TARGET_MYSQL_USER" "$TARGET_MYSQL_PASSWORD"
+else
+  fxTitle "🧪 Testing..."
+  mysql -u${MYSQL_USER} -p"${MYSQL_PASS}" -h localhost -e "SHOW DATABASES;"
+fi
 
 fxEndFooter
