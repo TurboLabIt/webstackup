7d54ab55db2ca8cbec35b19c184d0c0549c6cd5f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 23:17:14 +0200

    Update migrate.sh

--- my-app-template/scripts/migrate.sh
+++ my-app-template/scripts/migrate.sh
@@ -5,6 +5,8 @@
 
 source $(dirname $(readlink -f $0))/script_begin.sh
 
+fxHeader "🚕 migrate"
+
 fxCatastrophicError "migrate.sh is not ready!" dont-stop
 
 echo "🧙 for Magento: https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/magento/migrate.sh"
