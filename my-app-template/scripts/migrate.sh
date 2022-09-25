2eb76969b5e810e2d8e6b66fa808bfd5a9677834
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 16:53:43 +0200

    Create migrate.sh

---
+++ my-app-template/scripts/migrate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+## migrate placeholder
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/migrate.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+
+fxCatastrophicError "migrate.sh is not ready!" dont-stop
+
+echo "🧙 for Magento: https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/magento/migrate.sh"
+echo "📐 for Symfony: https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/symfony/migrate.sh"
+
+fxEndFooter failure
+exit
