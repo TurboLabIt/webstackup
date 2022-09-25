5cd29d90cf1ace2ad76a46b7bfdaca7c0418e1d6
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 20:54:55 +0200

    Delete index.php

--- config/php/index.php
+++
@@ -1,9 +0,0 @@
-﻿<html>
-<body>
-<h1>YES!</h1>
-<p><a href="https://github.com/TurboLabIt/webstackup">WEBSTACK.UP</a> installed this test page for you, and it worked!</p>
-
-<h1>Test PHP</h1>
-<p>Check the current timestamp here: => <strong><?php echo date("c") ?></strong></p>
-</body>
-</html>
