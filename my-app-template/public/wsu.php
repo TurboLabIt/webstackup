56b74fc19613e9ed7e26fbf3d582eb2cd904ae41
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 18:55:19 +0200

    Create wsu.php

---
+++ my-app-template/public/wsu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Webstackup test file</title>
+    <!--link rel="stylesheet" href="css/main.css" /-->
+    <!--link rel="icon" href="images/favicon.png" /-->
+  </head>
+
+  <body>
+    <!--script src="js/scripts.js"></script-->
+    
+    <h1>YES!</h1>
+    <p><a href="https://github.com/TurboLabIt/webstackup">WEBSTACK.UP</a> installed this test page for you, and it worked!</p>
+    
+    <h1>PHP test</h1>
+    <p>Check the current timestamp here: => <strong><?php echo date("c") ?></strong></p>
+  </body>
+</html>
