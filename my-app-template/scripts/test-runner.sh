36c6e3525b13c76dba798051435978fa8e793e4b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 16:56:36 +0200

    Create test-runner.sh

---
+++ my-app-template/scripts/test-runner.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+## test-runner placeholder
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/test-runner.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+
+fxCatastrophicError "test-runner.sh is not ready!" dont-stop
+
+## work in progress! Check https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/test-runner.sh !
+
+fxEndFooter failure
+exit
