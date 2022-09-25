fbff8af15f856f522f029197b6050a5297447399
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-09-25 17:55:02 +0200

    Update deploy-end.sh

--- script/filesystem/deploy-end.sh
+++ script/filesystem/deploy-end.sh
@@ -1,8 +1,7 @@
-if [ "$APP_ENV" = "prod" ] && [ -f ${SCRIPT_DIR}deploy_notify.sh ]; then
+if [ "$APP_ENV" = "prod" ] && [ -f "${SCRIPT_DIR}deploy_notify.sh" ]; then
   bash ${SCRIPT_DIR}notify.sh "🎉 Deploy completed for ${APP_NAME} on PRODUCTION ($HOSTNAME)" "$1"
 fi
 
-fxTitle "Removing lockfile ${LOCKFILE}..."
 removeLock ${LOCKFILE}
 
 source ${SCRIPT_DIR}/script_end.sh
