9081f2803aa54b37c89afbce46706eb3aeb695a2
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-02 20:55:36 +0200

    Update startup-isolated-ssh.sh

--- script/docker/startup-isolated-ssh.sh
+++ script/docker/startup-isolated-ssh.sh
@@ -4,9 +4,4 @@
 # This is your own copy of https://github.com/TurboLabIt/webstackup/blob/master/script/docker/startup-isolated-ssh.sh
 # Edit to your needs!
 
-if [ -f /usr/local/turbolab.it/zzalias/zzalias.sh ]; then
-  source /usr/local/turbolab.it/zzalias/zzalias.sh
-fi
-
-ssh-keygen -A
-exec /usr/sbin/sshd -D -e "$@"
+/usr/sbin/sshd -D
