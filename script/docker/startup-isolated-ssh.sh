f3336bc7a09c8b3f38f88df8f9b64a8fc875ab2f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-02 21:08:04 +0200

    Update startup-isolated-ssh.sh

--- script/docker/startup-isolated-ssh.sh
+++ script/docker/startup-isolated-ssh.sh
@@ -4,4 +4,16 @@
 # This is your own copy of https://github.com/TurboLabIt/webstackup/blob/master/script/docker/startup-isolated-ssh.sh
 # Edit to your needs!
 
+## server key generation
+ssh-keygen -A
+
+## disable password login via SSH
+if grep -qi webstackup "/etc/ssh/sshd_config"; then
+  
+  echo "" >> /etc/ssh/sshd_config
+  echo "## https://github.com/TurboLabIt/webstackup/blob/master/script/docker/startup-isolated-ssh.sh" >> /etc/ssh/sshd_config
+  echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+fi
+
+## start OpenSSH
 /usr/sbin/sshd -D
