03487e0a4c551886828076858b77c9ddfdf53f5d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-02 17:49:35 +0200

    Update startup-isolated-ssh.sh

--- script/docker/startup-isolated-ssh.sh
+++ script/docker/startup-isolated-ssh.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-## WEBSTACKUP DOCKER STARTUP SCRIPT FOR ISOLATED SSH
+## WEBSTACKUP DOCKER STARTUP SCRIPT FOR ISOLATED SSH ACCESS
 #
 # This is your own copy of https://github.com/TurboLabIt/webstackup/blob/master/script/docker/startup-isolated-ssh.sh
 # Edit to your needs!
 
-service ssh start
-
 if [ -f /usr/local/turbolab.it/zzalias/zzalias.sh ]; then
   source /usr/local/turbolab.it/zzalias/zzalias.sh
 fi
+
+ssh-keygen -A
+exec /usr/sbin/sshd -D -e "$@"
