b5fd7420fb99146e98e270492437ca5de239f3b1
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-02 18:50:32 +0200

    Update startup-isolated-ssh.sh

--- script/docker/startup-isolated-ssh.sh
+++ script/docker/startup-isolated-ssh.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 ## WEBSTACKUP DOCKER STARTUP SCRIPT FOR ISOLATED SSH ACCESS
 #
 # This is your own copy of https://github.com/TurboLabIt/webstackup/blob/master/script/docker/startup-isolated-ssh.sh
