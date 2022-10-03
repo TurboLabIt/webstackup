ded5de6c6d6a2c9a90bd3a138db467db77efe57d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-03 17:18:46 +0200

    Update notify.sh

--- my-app-template/scripts/notify.sh
+++ my-app-template/scripts/notify.sh
@@ -4,9 +4,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/notify.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-
-fxHeader "🔔 Notify"
-
-## Work in progress! Check https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/notify.sh
-
+source "${PRIVGEN_DIR}operations/webstackup/notify.sh"
 source $(dirname $(readlink -f $0))/script_end.sh
