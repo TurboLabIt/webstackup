5914705c524bf779632079c1a80ffbe15a2f204a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-10-12 12:55:25 +0200

    Update migrate.sh

--- script/frameworks/symfony/migrate.sh
+++ script/frameworks/symfony/migrate.sh
@@ -1,13 +1,23 @@
-#!/usr/bin/env bash
-## Standard Symfony migration routine by WEBSTACKUP
-# printTitle "💫 Copying Symfony scripts from webstackup..."
-# cp "${WEBSTACKUP_SCRIPT_DIR}frameworks/symfony/migrate.sh" "${SCRIPT_DIR}"
+## Run Symfony migration by WEBSTACKUP
+#
+# How to:
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/migrate.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/frameworks/symfony/migrate-starter.sh && sudo chmod u=rwx,go=rx scripts/migrate.sh
+#
+# 1. You should now git commit your copy
+#
+# Tip: after the first `deploy.sh`, you can `zzmigrate` directly
 
 SCRIPT_NAME=symfony-migrate
+fxHeader "🚕 migrate"
 
-source $(dirname $(readlink -f $0))/script_begin.sh
-printHeader "🚕 migrate"
+showPHPVer
 
-sudo -u "${EXPECTED_USER}" -H symfony console doctrine:migrations:migrate --no-interaction
+if [ -z "${PROJECT_DIR}" ] || [ ! -d "${PROJECT_DIR}" ]; then
+  fxCatastrophicError "📁 PROJECT_DIR not set"
+fi
 
-source $(dirname $(readlink -f $0))/script_end.sh
+cd "$PROJECT_DIR"
+
+wsuSymfony console doctrine:migrations:migrate --no-interaction
