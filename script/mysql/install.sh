4aa229fe4818fa2bd760b884c6b35fce5fa99851
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-11-06 15:36:13 +0100

    Update install.sh

--- script/mysql/install.sh
+++ script/mysql/install.sh
@@ -1,40 +1,47 @@
 #!/usr/bin/env bash
 ### AUTOMATIC MYSQL INSTALL BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/mysql/install.sh
 #
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/mysql/install.sh?$(date +%s) | sudo bash
+#
+# Based on: https://turbolab.it/1381
 
-echo ""
-echo -e "\e[1;46m ================ \e[0m"
-echo -e "\e[1;46m 🗃️ MYSQL INSTALL \e[0m"
-echo -e "\e[1;46m ================ \e[0m"
-
-if ! [ $(id -u) = 0 ]; then
-  echo -e "\e[1;41m This script must run as ROOT \e[0m"
-  exit
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
 fi
+## bash-fx is ready
 
-echo -e "\e[1;45m Removing previous version (if any) \e[0m"
-apt purge --auto-remove mysql* -y -qq
+fxHeader "💿 MySQL installer "
+rootCheck
 
-echo -e "\e[1;45m Setting up the repo... \e[0m"
-# https://dev.mysql.com/doc/mysql-apt-repo-quick-guide/en/#apt-repo-fresh-install
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3A79BD29
+fxTitle "Removing any old previous instance..."
+apt purge --auto-remove mysql* -y
+
+fxTitle "Import an official signing key..."
+curl https://raw.githubusercontent.com/TurboLabIt/webstackup/master/config/mysql/mysql.key | gpg --dearmor | sudo tee /usr/share/keyrings/mysql-archive-keyring.gpg >/dev/null
+
+fxTitle "Setting up the repo..."
+## https://dev.mysql.com/doc/mysql-apt-repo-quick-guide/en/#apt-repo-fresh-install
 
 echo "### webstackup" > /etc/apt/sources.list.d/webstackup.mysql.list
 echo "deb http://repo.mysql.com/apt/ubuntu/ $(lsb_release -sc) mysql-${MYSQL_VER}" >> /etc/apt/sources.list.d/webstackup.mysql.list
 echo "deb-src http://repo.mysql.com/apt/ubuntu/ $(lsb_release -sc) mysql-${MYSQL_VER}" >> /etc/apt/sources.list.d/webstackup.mysql.list
 echo "deb http://repo.mysql.com/apt/ubuntu/ $(lsb_release -sc) mysql-tools" >> /etc/apt/sources.list.d/webstackup.mysql.list
   
-echo -e "\e[1;45m Generating a random MySQL root password... \e[0m"
+fxTitle "Generating a random MySQL root password..."
 MYSQL_ROOT_PASSWORD="$(head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 19)"
 debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${MYSQL_ROOT_PASSWORD}"
 debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${MYSQL_ROOT_PASSWORD}"
 debconf-set-selections <<< "mysql-community-server mysql-server/default-auth-override select"
 
-echo -e "\e[1;45m Installing... \e[0m"
+fxTitle "Installing..."
 apt update -qq
 apt install mysql-server mysql-client -y -qq
   
-echo -e "\e[1;45m Enabling the custom config... \e[0m"
+fxTitle "Enabling the custom config..."
 cp "${WEBSTACKUP_INSTALL_DIR}config/mysql/mysql.cnf" "/etc/mysql/mysql.conf.d/webstackup.cnf"
 chmod u=rw,go=r /etc/mysql/mysql.conf.d/*.cnf
   
@@ -43,7 +50,7 @@ MYSQL_CREDENTIALS_FULLPATH="${MYSQL_CREDENTIALS_DIR}mysql.conf"
   
 if [ ! -e "${MYSQL_CREDENTIALS_FULLPATH}" ]; then
   
-  echo -e "\e[1;45m Writing MySQL root credentials to ${MYSQL_CREDENTIALS_FULLPATH}... \e[0m"
+  fxTitle "Writing MySQL root credentials to ${MYSQL_CREDENTIALS_FULLPATH}..."
   mkdir -p "$MYSQL_CREDENTIALS_DIR"
   echo "MYSQL_USER='root'" > "${MYSQL_CREDENTIALS_FULLPATH}"
   echo "MYSQL_PASSWORD='$MYSQL_ROOT_PASSWORD'" >> "${MYSQL_CREDENTIALS_FULLPATH}"
@@ -53,7 +60,7 @@ if [ ! -e "${MYSQL_CREDENTIALS_FULLPATH}" ]; then
   chmod u=rw,go= "${MYSQL_CREDENTIALS_FULLPATH}"
 fi
   
-echo -e "\e[1;45m $(cat "${MYSQL_CREDENTIALS_FULLPATH}") \e[0m"
+fxTitle "$(cat "${MYSQL_CREDENTIALS_FULLPATH}")"
   
 service mysql restart
 systemctl --no-pager status mysql
@@ -63,3 +70,5 @@ if [ -f /usr/local/turbolab.it/webstackup/script/mysql/maintentance.sh ]; then
 fi
 
 curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/mysql/mysqltuner.sh?$(date +%s) | sudo bash
+
+fxEndFooter
