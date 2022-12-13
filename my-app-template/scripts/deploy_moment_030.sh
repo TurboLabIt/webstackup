0ff6ce7ed23f2a64dfde97bdd0df27fe64b71dc9
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 15:13:59 +0100

    Create deploy_moment_030.sh

---
+++ my-app-template/scripts/deploy_moment_030.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## This script is executed during the deploy. See: https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/deploy_run.sh
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/deploy_moment_030.sh
+
+#fxTitle "📦 Setting up composer credentials..."
+#wsuComposer config --global http-basic.repo.magento.com myKey1 myKey2
