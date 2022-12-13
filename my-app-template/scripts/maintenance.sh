232680e13f452253de996c0658ed412b0ea630bd
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 17:30:40 +0100

    Update maintenance.sh

--- my-app-template/scripts/maintenance.sh
+++ my-app-template/scripts/maintenance.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
-## maintenance placeholder
+## Framework-based clear-cache by WEBSTACKUP
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/maintenance.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-
-fxHeader "🧰 Maintenance"
-
-fxCatastrophicError "maintenance.sh is not ready!" dont-stop
-
-echo "🧙 for Magento: replace this file with the one from https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/magento/maintenance.sh"
-echo "📐 for any project: replace this file with the one from https://github.com/TurboLabIt/webstackup/blob/master/script/nginx/maintenance.sh"
-
-fxEndFooter failure
-exit
+wsuSourceFrameworkScript maintenance "$1"
