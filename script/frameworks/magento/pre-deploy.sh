361426f3cdabdf3df7948ee2d746af4c5c575042
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 11:54:24 +0100

    Update pre-deploy.sh

--- script/frameworks/magento/pre-deploy.sh
+++ script/frameworks/magento/pre-deploy.sh
@@ -2,6 +2,27 @@ if [ ! -d "${MAGENTO_DIR}" ]; then
   return 0
 fi
 
+fxTitle "Installing required PHP extensions and support packages..."
+if [ ! -z "${PHP_VER}" ]; then
+
+  ## see: https://experienceleague.adobe.com/docs/commerce-operations/installation-guide/prerequisites/php-settings.html?mt=false#verify-installed-extensions
+
+  apt update
+  apt install \
+    php${PHP_VER}-bcmath php${PHP_VER}-ctype php${PHP_VER}-curl \
+    php${PHP_VER}-dom php${PHP_VER}-fileinfo php${PHP_VER}-gd \
+    php${PHP_VER}-iconv php${PHP_VER}-intl php${PHP_VER}-json \
+    php${PHP_VER}-xml php${PHP_VER}-mbstring php${PHP_VER}-mysql \
+    php${PHP_VER}-simplexml php${PHP_VER}-soap php${PHP_VER}-sockets \
+    php${PHP_VER}-xmlwriter php${PHP_VER}-xsl php${PHP_VER}-zip \
+    libxml2 openssl \
+  -y
+
+else
+
+  fxWarning "PHP_VER is undefined, skipping"
+fi
+
 N98_MAGERUN_FILE=/usr/local/bin/n98-magerun2
 
 if [ ! -f "${N98_MAGERUN_FILE}" ]; then
