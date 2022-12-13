c728e57294331356fdc2f3cab36ce254e7022a5e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 17:36:35 +0100

    Update reindex.sh

--- my-app-template/scripts/reindex.sh
+++ my-app-template/scripts/reindex.sh
@@ -4,12 +4,4 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/reindex.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-
-fxHeader "🗂️ reindex"
-
-fxCatastrophicError "reindex.sh is not ready!" dont-stop
-
-echo "🧙 for Magento: replace this file with the one from https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/magento/reindex.sh"
-
-fxEndFooter failure
-exit
+wsuSourceFrameworkScript reindex
