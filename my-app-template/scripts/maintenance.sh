553e555352b9354e4814e3dbeabb64c3022b03ad
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 17:43:55 +0100

    Update maintenance.sh

--- my-app-template/scripts/maintenance.sh
+++ my-app-template/scripts/maintenance.sh
@@ -4,4 +4,4 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/maintenance.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-wsuSourceFrameworkScript maintenance "$1"
+wsuSourceFrameworkScript maintenance "$@"
