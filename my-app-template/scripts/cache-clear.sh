4358524710d1db25051de1c1df16e8b4af8d866f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-13 17:28:08 +0100

    Update cache-clear.sh

--- my-app-template/scripts/cache-clear.sh
+++ my-app-template/scripts/cache-clear.sh
@@ -4,10 +4,4 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/clear-cache.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-WSU_MAP_FRAMEWORK_CACHECLEAR="${WEBSTACKUP_SCRIPT_DIR}frameworks/${PROJECT_FRAMEWORK}/cache-clear.sh"
-if [ -f "${WSU_MAP_FRAMEWORK_CACHECLEAR}" ]; then
-  source "${WEBSTACKUP_SCRIPT_DIR}frameworks/${PROJECT_FRAMEWORK}/cache-clear.sh"
-  source "${SCRIPT_DIR}/script_end.sh"
-else
-  fxInfo "No clear-cache script available for your ##${PROJECT_FRAMEWORK}## framework"
-fi
+wsuSourceFrameworkScript cache-clear
