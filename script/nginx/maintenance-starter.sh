11a51bc96d9b390bc0348afdeabd9a1de4b98c9d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-14 08:01:05 +0100

    Delete maintenance-starter.sh

--- script/nginx/maintenance-starter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-## Activate standard maintenance page by WEBSTACKUP
-# Install instruction: https://github.com/TurboLabIt/webstackup/blob/master/script/nginx/maintenance.sh
-
-source $(dirname $(readlink -f $0))/script_begin.sh
-source "${WEBSTACKUP_SCRIPT_DIR}nginx/maintenance.sh"
-source "${SCRIPT_DIR}/script_end.sh"
