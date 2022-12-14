60899d4cb4818c400cb6e53666ba18dc926810fd
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-14 10:22:36 +0100

    Create cron.sh

---
+++ my-app-template/scripts/cron.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## This is the main cron job for your framework. It runs here: 
+#    https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/cron
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/cron.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+wsuSourceFrameworkScript cron "$@"
