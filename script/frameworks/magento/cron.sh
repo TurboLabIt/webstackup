d073e99d730ab4a8d778c56199638a3b773455d7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-14 10:40:40 +0100

    Create cron.sh

---
+++ script/frameworks/magento/cron.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+## Standard Magento cron job by WEBSTACKUP
+#
+# How to:
+#
+# 1. set `PROJECT_FRAMEWORK=magento` in your project `script_begin.sh`
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/cron.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/my-app-template/scripts/cron.sh && sudo chmod u=rwx,go=rx scripts/cron.sh
+#
+# 1. You should now git commit your copy
+#
+# After the next `deploy.sh`, the related cron file https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/cron will be activated
+
+fxHeader "🧙🕰️ Magento cron"
+showPHPVer
+wsuMage cron:run
