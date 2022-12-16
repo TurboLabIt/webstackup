c27694db31a3e3d193dec7e0aea53962d6a6caee
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-16 09:12:38 +0100

    Create cli.sh

---
+++ my-app-template/scripts/cli.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## Framework-based bin/console by WEBSTACKUP
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/cli.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+wsuSourceFrameworkScript cli "$@"
