2967157b478e9079a64a9b3eab858c079b9da2a7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-23 10:26:33 +0100

    Update benchmark.sh

--- script/performance/benchmark.sh
+++ script/performance/benchmark.sh
@@ -27,5 +27,6 @@ iostat -d | grep -vi loop
 
 fxTitle "Disk (fio)"
 fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fiotest --filename=testfio --bs=4k --iodepth=64 --size=2G --readwrite=randrw --rwmixread=75
+rm -f "${HOME}testfio"
 
 fxEndFooter
