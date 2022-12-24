5bad914d50e09e0b0d82a1bbe62e834c3bc1e231
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-24 11:35:39 +0100

    Create install.sh

---
+++ script/node.js/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+### AUTOMATIC NODE.JS INSTALLER BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/node.js/install.sh
+#
+# Get the newest version of node here: https://github.com/nodesource/distributions/blob/master/README.md#debinstall
+#
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/node.js/install.sh?$(date +%s) | sudo NODEJS_VER=19 bash
+#
+# Based on: https://turbolab.it/
+
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "💿 Node.js installer"
+rootCheck
+
+if [ -z "${NODEJS_VER}" ]; then
+  fxCatastrophicError "NODEJS_VER is undefined! Cannot determine which version of Node.js to install"
+fi
+
+
+
+
+
+fxEndFooter
