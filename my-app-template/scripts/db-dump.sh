c1b1f3c19f5ae0ef8b0b19aaeeff1f817f8b0eda
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-03 18:11:11 +0100

    Create db-dump.sh

---
+++ my-app-template/scripts/db-dump.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## Framework-based database dump by WEBSTACKUP
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/db-dump.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+wsuSourceFrameworkScript db-dump
