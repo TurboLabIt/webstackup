cfd4ff60b54dd63be07c41dd46aec654b0bed39a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-03 23:32:33 +0100

    Update db-load.sh

--- my-app-template/scripts/db-load.sh
+++ my-app-template/scripts/db-load.sh
@@ -4,4 +4,8 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/db-load.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
+
+DB_DUMP_FILE_PATH=${DB_DUMP_DIR}db_${APP_NAME}-prod_db-dump-sh.sql.gz
+SKIP_POST_LOAD_QUERY=0
+
 wsuSourceFrameworkScript db-load
