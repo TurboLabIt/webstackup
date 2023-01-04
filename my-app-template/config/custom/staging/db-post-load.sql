409ee3f9f52ef78530fd617af931b26787eeef10
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-04 14:46:21 +0100

    Create db-post-load.sql

---
+++ my-app-template/config/custom/staging/db-post-load.sql
@@ -0,0 +1,19 @@
+## 🚨 WARNING 🚨
+#
+# This file is under version control!
+# DO NOT EDIT DIRECTLY - If you do, you'll loose your changes!
+#
+# The original file is in `/var/www/my-app/config/custom/staging/`
+#
+# You MUST:
+#
+# 1. edit the original file on you PC
+# 2. Git-commit+push the changes
+# 3. run `sudo bash /var/www/my-app/scripts/deploy.sh`
+#
+# ⚠️ This file is for the STAGING env only ⚠️
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/staging/db-post-load.sql
+
+#UPDATE `core_config_data` SET `value` = 'https://next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
+#UPDATE `wp_options` SET `option_value` = 'https://next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
