8dbe005d37717d6f10078fdb714ea8f012e7544b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-04 14:13:11 +0100

    Update db-load.sh

--- script/frameworks/magento/db-load.sh
+++ script/frameworks/magento/db-load.sh
@@ -29,12 +29,14 @@ wsuN98MageRun db:import "${DB_DUMP_FILE_PATH}" --drop --compression=gzip
 fxTitle "⚙️ Running db-post-load SQL queries..."
 if [ "${SKIP_POST_LOAD_QUERY}" != 1 ]; then
 
-
   fxTitle "⚙️ Running SQL query for staging..."
   SQL_STAGING=${PROJECT_DIR}config/custom/staging/db-post-load.sql
   if [ -f "${SQL_STAGING}" ]; then
-    wsuMysql < "${SQL_STAGING}"
+
+    wsuN98MageRun db:import "${SQL_STAGING}"
+
   else
+
     fxWarning "##$SQL_STAGING## not found, skipping"
   fi
 
@@ -42,10 +44,15 @@ if [ "${SKIP_POST_LOAD_QUERY}" != 1 ]; then
   fxTitle "⚙️ Running SQL query for dev..."
   SQL_DEV=${PROJECT_DIR}config/custom/dev/db-post-load.sql
   if [ "${APP_ENV}" = "dev" ] && [ ! -f "${SQL_DEV}" ]; then
+
     fxWarning "##$SQL_DEV## not found, skipping"
+
   elif [ "${APP_ENV}" = "dev" ] && [ -f "${SQL_DEV}" ]; then
-    wsuMysql < "${SQL_DEV}"
+
+    wsuN98MageRun db:import "${SQL_DEV}"
+
   else
+
     fxInfo "APP_ENV is ##${APP_ENV}##, skipping"
   fi
 
