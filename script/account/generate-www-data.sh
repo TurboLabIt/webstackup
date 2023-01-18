f0df1d7fc9870cc7d357643bfecd24cb5661b7f2
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-18 13:11:21 +0100

    Update generate-www-data.sh

--- script/account/generate-www-data.sh
+++ script/account/generate-www-data.sh
@@ -48,8 +48,8 @@ if ! id "webstackup" &>/dev/null; then
   
   sudo -u webstackup -H ssh-keygen -t rsa -N "" -C "webstackup on $(hostname) by generate-www-data.sh" -f /home/webstackup/.ssh/id_rsa
   
-  sudo -u webstackup -H ssh-keyscan -t rsa github.com > /home/webstackup/.ssh/known_hosts
-  sudo -u webstackup -H ssh-keyscan -t rsa ssh-keyscan -t rsa bitbucket.org >> /home/webstackup/.ssh/known_hosts
+  ssh-keyscan -t rsa github.com > /home/webstackup/.ssh/known_hosts
+  ssh-keyscan -t rsa bitbucket.org >> /home/webstackup/.ssh/known_hosts
   chmod u=rw,go= /home/webstackup/.ssh/known_hosts
   chown webstackup:www-data /home/webstackup/.ssh/known_hosts
   
