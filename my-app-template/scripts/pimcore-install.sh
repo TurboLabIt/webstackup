ef2b2e527f93b2239c7e9e033d8404d4d8ad1aeb
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-19 22:43:02 +0100

    Update pimcore-install.sh

--- my-app-template/scripts/pimcore-install.sh
+++ my-app-template/scripts/pimcore-install.sh
@@ -6,17 +6,18 @@
 source $(dirname $(readlink -f $0))/script_begin.sh
 
 PIMCORE_SITE_NAME="My App Name"
+SITE_URL=https://my-app.com
 PIMCORE_LOCALE=it_IT
 PIMCORE_ADMIN_USERNAME=my-name
 PIMCORE_ADMIN_EMAIL=admin@my-app.com
 PIMCORE_ADMIN_NEW_SLUG=my-app$(date +"%Y")
 
+## 💡 You can get these 👇🏻 from the app-specific /etc/turbolab.it/mysql-my-app.conf file
 MYSQL_USER=my-app
 MYSQL_DB_NAME=my-app
-## ☠️☠️ DON'T GIT COMMIT THE PASSWORD ☠️☠️ ##
 MYSQL_PASSWORD=
 MYSQL_HOST=localhost
-SITE_URL=https://my-app.com
+## ☠️☠️ DON'T GIT COMMIT THE PASSWORD ☠️☠️ ##
 
 source ${WEBSTACKUP_SCRIPT_DIR}frameworks/pimcore/install.sh
 
