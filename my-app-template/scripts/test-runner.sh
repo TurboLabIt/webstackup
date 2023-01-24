15e6b5d1a1ed45973c11637d174c2e9d74137606
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-24 15:28:24 +0100

    Update test-runner.sh

--- my-app-template/scripts/test-runner.sh
+++ my-app-template/scripts/test-runner.sh
@@ -3,22 +3,26 @@
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/test-runner.sh
 
-source $(dirname $(readlink -f $0))/script_begin.sh
-# if you're testing a package, use this instead:
-#source "/usr/local/turbolab.it/webstackup/script/base.sh"
-#APP_NAME=MyPackageName
-#EXPECTED_USER=$(logname)
+TESTR_SCRIPT_BEGIN=$(dirname $(readlink -f $0))/script_begin.sh
+if [ -f "${TESTR_SCRIPT_BEGIN}" ]; then
+  source "${TESTR_SCRIPT_BEGIN}"
+else
+  source "/usr/local/turbolab.it/webstackup/script/base.sh"
+  APP_NAME=my-app
+  EXPECTED_USER=$(logname)
+fi
 
 fxHeader "🧪 ${APP_NAME} Test Runner"
 
-# ... pre-test stuff ...
-
 # https://github.com/TurboLabIt/webstackup/tree/master/script/php/test-runner-package.sh
-source "/usr/local/turbolab.it/webstackup/script/php/test-runner-package.sh"
+source "${WEBSTACKUP_SCRIPT_DIR}php/test-runner-package.sh"
 
 fxTitle "🧹 Cleaning up..."
 #rm -rf /tmp/any-temp-dir
 
-source $(dirname $(readlink -f $0))/script_end.sh
-# if you're testing a package, just use this instead:
-#fxEndFooter
+TESTR_SCRIPT_END=$(dirname $(readlink -f $0))/script_end.sh
+if [ -f "${TESTR_SCRIPT_END}" ]; then
+  source "${TESTR_SCRIPT_END}"
+else
+  fxEndFooter
+fi
