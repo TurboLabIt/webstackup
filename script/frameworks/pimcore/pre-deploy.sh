592bd677ef589ca23dc797036a113f58007375a8
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-24 10:32:17 +0100

    Update pre-deploy.sh

--- script/frameworks/pimcore/pre-deploy.sh
+++ script/frameworks/pimcore/pre-deploy.sh
@@ -5,7 +5,7 @@ if [ ! -z "${PHP_VER}" ]; then
   apt install \
     php${PHP_VER}-bcmath php${PHP_VER}-ctype php${PHP_VER}-curl \
     php${PHP_VER}-dom php${PHP_VER}-fileinfo php${PHP_VER}-gd \
-    php${PHP_VER}-iconv php${PHP_VER}-intl php${PHP_VER}-json \
+    php${PHP_VER}-iconv php${PHP_VER}-intl \
     php${PHP_VER}-xml php${PHP_VER}-mbstring php${PHP_VER}-mysql \
     php${PHP_VER}-simplexml php${PHP_VER}-soap php${PHP_VER}-sockets \
     php${PHP_VER}-xmlwriter php${PHP_VER}-xsl php${PHP_VER}-zip \
