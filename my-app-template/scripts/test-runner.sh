d34f306c777c1e684edcaa3f325b0f1674318c34
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-24 14:19:51 +0100

    Update test-runner.sh

--- my-app-template/scripts/test-runner.sh
+++ my-app-template/scripts/test-runner.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
-## Run phpunit tests of the project
+## Run the project tests and check the result
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/test-runner.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-# if you're testing a package, just use this instead:
+# if you're testing a package, use this instead:
 #source "/usr/local/turbolab.it/webstackup/script/base.sh"
 #APP_NAME=MyPackageName
+#EXPECTED_USER=$(logname)
+
+fxHeader "🧪 ${APP_NAME} Test Runner"
+
+# ... pre-test stuff ...
 
 # https://github.com/TurboLabIt/webstackup/tree/master/script/php/test-runner-package.sh
 source "/usr/local/turbolab.it/webstackup/script/php/test-runner-package.sh"
@@ -14,4 +19,6 @@ source "/usr/local/turbolab.it/webstackup/script/php/test-runner-package.sh"
 fxTitle "🧹 Cleaning up..."
 #rm -rf /tmp/any-temp-dir
 
-fxEndFooter
+source $(dirname $(readlink -f $0))/script_end.sh
+# if you're testing a package, just use this instead:
+#fxEndFooter
