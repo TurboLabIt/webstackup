5b7ec4a8cfa5310ceaee8ef381876eed6d3a6b60
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-28 15:48:35 +0100

    Update db-load.sh

--- my-app-template/scripts/db-load.sh
+++ my-app-template/scripts/db-load.sh
@@ -5,10 +5,18 @@
 
 source $(dirname $(readlink -f $0))/script_begin.sh
 
+## for Magento
 DB_DUMP_FILE_PATH=${DB_DUMP_DIR}db_${APP_NAME}-prod_db-dump-sh.sql.gz
+
+## for others (zzmysqldump)
+DB_DUMP_FILE_PATH=${DB_DUMP_DIR}db_${APP_NAME}_$(date +'%u').sql.7z
+
+## local database name to import into (zzmysqldump only)
+MYSQL_DB_NAME=${APP_NAME}_shared
+
+## options
 SKIP_POST_LOAD_QUERY=0
 
 wsuSourceFrameworkScript db-load
 
 source "${SCRIPT_DIR}script_end.sh"
-
