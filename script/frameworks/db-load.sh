49fabcebb6a8031680b817312345b41beda79073
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-28 12:46:07 +0100

    Create db-load.sh

---
+++ script/frameworks/db-load.sh
@@ -0,0 +1,16 @@
+## Framework-initiated database dump via zzmysqldump
+
+fxHeader "🗄️ Database loader"
+
+echo "🗄️ DB_DUMP_FILE_PATH:    ##${DB_DUMP_FILE_PATH}##"
+echo "📛 MYSQL_DB_NAME:        ##${MYSQL_DB_NAME}##"
+echo "⚙️ SKIP_POST_LOAD_QUERY:  ##${SKIP_POST_LOAD_QUERY}#"
+
+fxEnvNotProd
+showPHPVer
+
+cd "${PROJECT_DIR}backup/db-dumps"
+
+sudo zzmysqlimp "${DB_DUMP_FILE_PATH}" "${MYSQL_DB_NAME}"
+
+source "${WEBSTACKUP_SCRIPT_DIR}frameworks/db-load-after.sh"
