95d4ab18012f4e6973579467a7493cf2deb5c379
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-28 09:36:44 +0100

    Create db-dump.sh

---
+++ script/frameworks/pimcore/db-dump.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+## Standard PimCore db-dump routine by WEBSTACKUP
+#
+# How to:
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/db-dump.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/my-app-template/scripts/db-dump.sh && sudo chmod u=rwx,go=rx scripts/db-dump.sh
+#
+# 1. You should now git commit your copy
+
+source "${WEBSTACKUP_SCRIPT_DIR}frameworks/db-dump.sh"
