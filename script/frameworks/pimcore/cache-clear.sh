ce680570f04e73aa6b3720c361b8b1034863a41e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-06 15:20:39 +0100

    Update cache-clear.sh

--- script/frameworks/pimcore/cache-clear.sh
+++ script/frameworks/pimcore/cache-clear.sh
@@ -1,3 +1,55 @@
 #!/usr/bin/env bash
 ## Standard Pimcore cache-clearing routine by WEBSTACKUP
-source /usr/local/turbolab.it/webstackup/script/frameworks/symfony/cache-clear.sh
+
+fxHeader "♾🧹 Pimcore cache-clear"
+
+showPHPVer
+
+if [ -z "${PROJECT_DIR}" ] || [ ! -d "${PROJECT_DIR}" ]; then
+  fxCatastrophicError "📁 PROJECT_DIR not set"
+fi
+
+if [ "$1" = "fast" ]; then
+  FAST_CACHE_CLEAR=1
+fi
+
+cd "$PROJECT_DIR"
+
+if [ -z "${FAST_CACHE_CLEAR}" ]; then
+
+  fxTitle "⚙️ Stopping services.."
+  sudo nginx -t && sudo service nginx stop && sudo service ${PHP_FPM} stop
+
+  fxTitle "🧹 Removing Pimcore cache folder..."
+  sudo rm -rf "${PROJECT_DIR}var/cache"
+  
+  fxTitle "🚚 Migrating..."
+  wsuSymfony console doctrine:migrations:migrate --no-interaction
+
+else
+
+  fxTitle "📐 Symfony cache folder NOT removed (fast mode)"
+fi
+
+
+fxTitle "↗ Pushing current PHP classes to database..."
+# https://pimcore.com/docs/pimcore/current/Development_Documentation/Deployment/Deployment_Tools.html
+wsuSymfony console pimcore:deployment:classes-rebuild --create-classes
+
+
+fxTitle "🌊 Pimcore cache:clear..."
+# https://pimcore.com/docs/pimcore/current/Development_Documentation/Deployment/Deployment_Tools.html#page_Potentially-useful-commands
+wsuSymfony console pimcore:cache:clear
+wsuSymfony console cache:clear
+
+
+if [ -z "${FAST_CACHE_CLEAR}" ]; then
+
+  fxTitle "⚙️ Restarting services.."
+  sudo nginx -t && sudo service ${PHP_FPM} restart && sudo service nginx restart
+
+else
+
+  fxTitle "🌊 PHP OPcache clear..."
+  wsuOpcacheClear
+fi
