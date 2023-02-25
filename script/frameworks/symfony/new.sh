175674434f4a8f2653d3dcb4814bf7a8a323ac5a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-25 01:14:38 +0100

    Update new.sh

--- script/frameworks/symfony/new.sh
+++ script/frameworks/symfony/new.sh
@@ -9,17 +9,17 @@ rm -rf "${WSU_TMP_DIR}"
 mkdir -p "${WSU_TMP_DIR}"
 cd "${WSU_TMP_DIR}"
 
-symfony new ${APP_NAME}
+wsuSymfony new ${APP_NAME}
 cd ${APP_NAME}
 
 curl -O https://raw.githubusercontent.com/TurboLabIt/webdev-gitignore/master/.gitignore
 
-symfony composer require \
+wsuSymfony composer require \
   twig cache asset \
   doctrine mailer webpack-encore-bundle \
   stof/doctrine-extensions-bundle
   
-symfony composer require maker debug --dev
+wsuSymfony composer require maker debug --dev
 
 echo "
 stof_doctrine_extensions:
@@ -29,20 +29,20 @@ stof_doctrine_extensions:
 " > /dev/null
 #config/packages/stof_doctrine_extensions.yml
 
-symfony composer config repositories.TurboLabIt/TLIBaseBundle git https://github.com/TurboLabIt/TLIBaseBundle.git
-symfony composer require turbolabit/tli-base-bundle:dev-master
+wsuSymfony composer config repositories.TurboLabIt/TLIBaseBundle git https://github.com/TurboLabIt/TLIBaseBundle.git
+wsuSymfony composer require turbolabit/tli-base-bundle:dev-master
 
-composer config repositories.TurboLabIt/php-foreachable git https://github.com/TurboLabIt/php-foreachable.git
-composer require turbolabit/php-foreachable:dev-main
+wsuSymfony config repositories.TurboLabIt/php-foreachable git https://github.com/TurboLabIt/php-foreachable.git
+wsuSymfony require turbolabit/php-foreachable:dev-main
 
-composer config repositories.TurboLabIt/php-encryptor git https://github.com/TurboLabIt/php-encryptor.git
-composer require turbolabit/php-encryptor:dev-main
+wsuSymfony config repositories.TurboLabIt/php-encryptor git https://github.com/TurboLabIt/php-encryptor.git
+wsuSymfony require turbolabit/php-encryptor:dev-main
 
-composer config repositories.TurboLabIt/php-doctrine-runtime-manager git https://github.com/TurboLabIt/php-doctrine-runtime-manager.git
-composer require turbolabit/php-doctrine-runtime-manager:dev-main
+wsuSymfony config repositories.TurboLabIt/php-doctrine-runtime-manager git https://github.com/TurboLabIt/php-doctrine-runtime-manager.git
+wsuSymfony require turbolabit/php-doctrine-runtime-manager:dev-main
 
-symfony composer config repositories.TurboLabIt/BaseCommand git https://github.com/TurboLabIt/php-symfony-basecommand.git
-symfony composer require turbolabit/php-symfony-basecommand:dev-main
+wsuSymfony composer config repositories.TurboLabIt/BaseCommand git https://github.com/TurboLabIt/php-symfony-basecommand.git
+wsuSymfony composer require turbolabit/php-symfony-basecommand:dev-main
 
 rm -rf .git
 
