7c3675786c0d5f9d92a2a61141cfcffd00de1a46
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-25 01:07:15 +0100

    Update symfony-install.sh

--- my-app-template/scripts/symfony-install.sh
+++ my-app-template/scripts/symfony-install.sh
@@ -4,5 +4,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/symfony-install.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-source ${WEBSTACKUP_SCRIPT_DIR}frameworks/symfony-install.sh
+source ${WEBSTACKUP_SCRIPT_DIR}frameworks/symfony/symfony-install.sh
 source "${SCRIPT_DIR}/script_end.sh"
