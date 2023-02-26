a883ea415a5dd6a00025bdd66c7c0849d50f5a70
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-27 00:41:18 +0100

    Create install.sh

---
+++ script/frameworks/wordpress/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+### AUTOMATIC WP-CLI INSSTALLER BY WEBSTACK.UP
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/frameworks/wordpress/install.sh?$(date +%s) | sudo bash
+#
+# Source:
+
+
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+
+fxHeader "💿 WordPress-CLI installer "
+rootCheck
+
+
+fxTitle "Removing any old previous instance..."
+sudo rm -f /usr/local/bin/wp-cli
+
+curl -o /usr/local/bin/wp-cli https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod u=rwx,go=rx /usr/local/bin/wp-cli
+
+fxEndFooter
