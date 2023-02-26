c2e1a24d23299f41bbee1d9977eef05e600252a1
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-26 22:57:52 +0100

    Update install.sh

--- script/frameworks/symfony/install.sh
+++ script/frameworks/symfony/install.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
-### Automatic symfony-cli install by WEBSTACK.UP
+### AUTOMATIC SYMFONY-CLI INSSTALLER BY WEBSTACK.UP
 # sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/frameworks/symfony/install.sh?$(date +%s) | sudo bash
 #
+# Source: https://symfony.com/download
 
-echo ""
-echo -e "\e[1;46m ================== \e[0m"
-echo -e "\e[1;46m 📐 SYMFONY INSTALL \e[0m"
-echo -e "\e[1;46m ================== \e[0m"
 
-sudo rm -f /usr/local/bin/symfony
-sudo rm -f /usr/local/bin/__symfony*
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
 
+
+fxHeader "💿 Symfony-CLI installer "
+rootCheck
+
+
+fxTitle "Removing any old previous instance..."
+apt purge --auto-remove symfony* -y
+sudo rm -f /usr/local/bin/*symfony*
+
+
+fxTitle "Installing pre-requisites..."
 sudo apt update && sudo apt install ca-certificates apt-transport-https -y
 
-## https://symfony.com/download
+
+fxTitle "Installing Symfony from cloudsmith.io..."
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 sudo apt install symfony-cli -y
