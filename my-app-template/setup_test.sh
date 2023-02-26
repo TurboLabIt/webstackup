1ed20cff7052eac2c9fedf9b886a68e8277d8b69
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-26 17:15:33 +0100

    Update setup_test.sh

--- my-app-template/setup_test.sh
+++ my-app-template/setup_test.sh
@@ -10,7 +10,7 @@ export GIT_CLONE_BRANCH=dev
 export WSU_MAP_NAME="My test app $(date +%s)"
 export WSU_MAP_APP_NAME=wsu-map-test-$(date +%s)
 export WSU_MAP_DOMAIN=${WSU_MAP_APP_NAME}.io
-export WSU_MAP_DEPLOY_TO_PATH=/var/www/$WSU_MAP_APP_NAME
+export WSU_MAP_DEPLOY_TO_PATH=/var/www/zane/$WSU_MAP_APP_NAME
 
 export WSU_MAP_FRAMEWORK=pimcore
 export WSU_MAP_RUN_FRAMEWORK_INSTALLER=yes
