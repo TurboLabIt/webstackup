eadbffaba6a119239a7f14dbb966bdd1cf92dcab
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-26 11:44:30 +0100

    Update new.sh

--- script/frameworks/pimcore/new.sh
+++ script/frameworks/pimcore/new.sh
@@ -87,14 +87,15 @@ PCINST_SITE_DOMAIN=${PCINST_SITE_DOMAIN%*/}
 
 
 fxTitle "Running the downloaded Pimcore installer from vendor..." 
-sudo -u $EXPECTED_USER -H XDEBUG_MODE=off \ 
+echo "sudo -u $EXPECTED_USER -H XDEBUG_MODE=off \ 
   PIMCORE_INSTALL_ADMIN_USERNAME="${PIMCORE_ADMIN_USERNAME}" \
   PIMCORE_INSTALL_ADMIN_PASSWORD="${PCINST_FIRST_ADMIN_PASSWORD}" \
   PIMCORE_INSTALL_MYSQL_USERNAME="${MYSQL_USER}" \
   PIMCORE_INSTALL_MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
   ${PHP_CLI} vendor/bin/pimcore-install \
   --mysql-host-socket "${MYSQL_HOST}" --mysql-database "${MYSQL_DB_NAME}" \
-  --no-interaction
+  --no-interaction"
+exit
 
 
 fxTitle "Adding .gitignore..."
