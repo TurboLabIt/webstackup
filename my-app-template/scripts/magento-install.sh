e79202e3c84e2c664a6a67eaa4008634ea05a2d0
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-27 08:55:34 +0100

    Create magento-install.sh

---
+++ my-app-template/scripts/magento-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+## Pimcore installer
+#
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/pimcore-install.sh
+
+source $(dirname $(readlink -f $0))/script_begin.sh
+
+SITE_URL=https://my-app.com/
+MAGENTO_ADMIN_USERNAME=my-name
+MAGENTO_ADMIN_EMAIL=admin@my-app.com
+MAGENTO_ADMIN_NEW_SLUG=my-app$(date +"%Y")
+MAGENTO_LOCALE=it_IT
+MAGENTO_CURRENCY=EUR
+MAGENTO_TIMEZONE=Europe/Rome
+
+source "/etc/turbolab.it/mysql-usr_my-app.conf"
+
+source ${WEBSTACKUP_SCRIPT_DIR}frameworks/pimcore/new.sh
+
+source "${SCRIPT_DIR}/script_end.sh"
