5439ca27a0b4673f605eab2ca6f2ce13365249d7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-02-27 08:57:55 +0100

    Update magento-install.sh

--- my-app-template/scripts/magento-install.sh
+++ my-app-template/scripts/magento-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## Pimcore installer
 #
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/pimcore-install.sh
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/magento-install.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
 
