8dfe6674482e6be7a517a1831f8a328725322a7f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-04 21:37:52 +0100

    Update install.sh

--- script/php/install.sh
+++ script/php/install.sh
@@ -17,15 +17,13 @@ fi
 fxHeader "💿 PHP installer"
 rootCheck
 
-
 if [ -z "${PHP_VER}" ]; then
   fxCatastrophicError "PHP_VER is undefined! Cannot determine which version of PHP to install"
 fi
 
-
 ## checking compatibility
 # https://github.com/TurboLabIt/webstackup/issues/15
-local PHP_VER_CLI="${PHP_VER}"
+PHP_VER_CLI="${PHP_VER}"
 source <(curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/webstackup.default.conf)
 fxRequireCompatbileUbuntuVersion "${COMPATIBLE_OS_VERSIONS}"
 PHP_VER="${PHP_VER_CLI}"
