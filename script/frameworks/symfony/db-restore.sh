3d523685500e2220fb98dcfc3e45dcdc44dadb44
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-07 15:08:02 +0100

    Update and rename db-load.sh to db-restore.sh

---
+++ script/frameworks/symfony/db-restore.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+## Standard Symfony database dump restore routine by WEBSTACKUP
+#
+# How to:
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/db-restore.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/my-app-template/scripts/db-restore.sh && sudo chmod u=rwx,go=rx scripts/db-restore.sh
+#
+# 1. You should now git commit your copy
+
+source "${WEBSTACKUP_SCRIPT_DIR}frameworks/db-restore.sh"
