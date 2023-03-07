8563c7ae28252174fe039d441df6cd0cd1f43066
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-07 16:41:46 +0100

    Update cache-clear.sh

--- script/frameworks/magento/cache-clear.sh
+++ script/frameworks/magento/cache-clear.sh
@@ -145,7 +145,7 @@ fi
 wsuMage cache:flush
 
 
-#fxTitle "🐧 Setting permissions..."
+fxTitle "🐧 Setting permissions..."
 sudo -b find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} + > /dev/null 2>&1
 sudo -b find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} + > /dev/null 2>&1
 
