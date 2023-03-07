cd9cde370c2614c42801314c374d906084699aa9
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-07 15:10:08 +0100

    Update and rename db-post-load.sql to db-post-restore.sql

--- my-app-template/config/custom/staging/db-post-restore.sql
+++ my-app-template/config/custom/staging/db-post-restore.sql
@@ -1,11 +1,11 @@
-## Post-database load queries
+## Post-database restore queries
 #
-# The following queries are executed automatically by `scripts/db-load.sh`
-# after the database dump is loaded.
+# The following queries are executed automatically by `scripts/db-restore.sh`
+# after the database dump is restored.
 #
 # ⚠️ This file runs both on STAGING and on DEV env ⚠️
 #
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/staging/db-post-load.sql
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/staging/db-post-restore.sql
 
 #UPDATE `core_config_data` SET `value` = 'https://next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
 #UPDATE `wp_options` SET `option_value` = 'https://next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
