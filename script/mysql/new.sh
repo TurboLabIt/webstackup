ec7e7250ffe49675b8f6c85713ee41d96457a020
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-11 17:44:07 +0100

    Update new.sh

--- script/mysql/new.sh
+++ script/mysql/new.sh
@@ -12,53 +12,18 @@ if [ ! -e "${MYSQL_CREDENTIALS_FILE}" ]; then
   fxCatastrophicError "Credentials file ${MYSQL_CREDENTIALS_FILE} not found!"
 fi
 
-
 source "${WEBSTACKUP_SCRIPT_DIR}mysql/ask-credentials.sh"
 
-
-fxTitle "🧺 Database name"
-while [ -z "$NEW_MYSQL_DB_NAME" ]; do
-
-  echo "🤖 Provide the database name or hit Enter for ##${NEW_MYSQL_DB_NAME_DEFAULT}##"
-  read -p ">> " NEW_MYSQL_DB_NAME  < /dev/tty
-  if [ -z "$NEW_MYSQL_DB_NAME" ]; then
-    NEW_MYSQL_DB_NAME=$NEW_MYSQL_DB_NAME_DEFAULT
-  fi
-
-done
-
-fxOK "Swell, the database will be named ##$NEW_MYSQL_USER##"
-
-
-fxTitle "🚀 Preview..."
-fxMessage "AppName:     ##$NEW_MYSQL_APP_NAME##"
-fxMessage "User:        ##$NEW_MYSQL_USER##"
-fxMessage "Password:    ##$NEW_MYSQL_PASSWORD##"
-fxMessage "Host:        ##$NEW_MYSQL_HOST##"
-fxMessage "DB Name:     ##$NEW_MYSQL_DB_NAME##"
-fxCountdown 5
-echo ""
-
-
-
-fxTitle "📦 Installing prerequisites..."
-if [ -z "$(command -v mysql)" ]; then
-  apt update && apt install mysql-client -y
-else
-  fxInfo "prerequisite(s) already installed"
-fi
-
-
 fxTitle "🧔 Creating the user..."  
-wsuMysql -e "CREATE USER '$NEW_MYSQL_USER'@'%' IDENTIFIED BY '$NEW_MYSQL_PASSWORD';"
+wsuMysql -e "CREATE USER '$NEW_MYSQL_USER'@'$NEW_MYSQL_USER_FROM_HOST' IDENTIFIED BY '$NEW_MYSQL_PASSWORD';"
 
 fxTitle "🧺 Creating the database..."
 wsuMysql -e "CREATE DATABASE \`$NEW_MYSQL_DB_NAME\` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;"
 
 fxTitle "🔑 Granting privileges..."
-wsuMysql -e "GRANT ALL PRIVILEGES ON \`${NEW_MYSQL_APP_NAME//_/\\_}%\`.* TO '$NEW_MYSQL_USER'@'%';"
-wsuMysql -e "GRANT ALL PRIVILEGES ON \`${NEW_MYSQL_DB_NAME}\`.* TO '$NEW_MYSQL_USER'@'%';"
-wsuMysql -e "GRANT RELOAD, PROCESS ON *.* TO '$NEW_MYSQL_USER'@'%';"
+wsuMysql -e "GRANT ALL PRIVILEGES ON \`${NEW_MYSQL_APP_NAME//_/\\_}%\`.* TO '$NEW_MYSQL_USER'@'$NEW_MYSQL_USER_FROM_HOST';"
+wsuMysql -e "GRANT ALL PRIVILEGES ON \`${NEW_MYSQL_DB_NAME}\`.* TO '$NEW_MYSQL_USER'@'$NEW_MYSQL_USER_FROM_HOST';"
+wsuMysql -e "GRANT RELOAD, PROCESS ON *.* TO '$NEW_MYSQL_USER'@'$NEW_MYSQL_USER_FROM_HOST';"
 wsuMysql -e "FLUSH PRIVILEGES;"
 
 wsuMysqlStoreCredentials "${NEW_MYSQL_APP_NAME}" "${NEW_MYSQL_USER}" "${NEW_MYSQL_PASSWORD}" "${MYSQL_HOST}" "${NEW_MYSQL_DB_NAME}"
