dfb6d200c21d2c90e83ca002b6eaea4ca8bd0b3f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-14 09:43:39 +0100

    Update db-dump.sh

--- my-app-template/scripts/db-dump.sh
+++ my-app-template/scripts/db-dump.sh
@@ -4,5 +4,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/db-dump.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-wsuSourceFrameworkScript db-dump
+wsuSourceFrameworkScript db-dump "${APP_NAME}"
 source "${SCRIPT_DIR}script_end.sh"
