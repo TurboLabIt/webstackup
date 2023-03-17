0676c5c0b2c3627e84e686732ad42d541041a058
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 16:13:25 +0100

    Update deploy_run.sh

--- my-app-template/scripts/deploy_run.sh
+++ my-app-template/scripts/deploy_run.sh
@@ -5,5 +5,6 @@
 
 SCRIPT_NAME=deploy
 source $(dirname $(readlink -f $0))/script_begin.sh
+## https://github.com/TurboLabIt/webstackup/blob/master/script/filesystem/deploy_run.sh
 source "${WEBSTACKUP_SCRIPT_DIR}filesystem/deploy_run.sh"
 source "${SCRIPT_DIR}script_end.sh"
