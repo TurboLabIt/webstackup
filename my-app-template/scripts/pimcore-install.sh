c8523745004014a154850c16846017b83bf6d731
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:39:04 +0100

    Update pimcore-install.sh

--- my-app-template/scripts/pimcore-install.sh
+++ my-app-template/scripts/pimcore-install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## Pimcore installer
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/pimcore-install.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
