1b19234c7e4196dc990d38490dfe609ac7100cb1
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:36:49 +0100

    Update deploy.sh

--- my-app-template/scripts/deploy.sh
+++ my-app-template/scripts/deploy.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-## Run this script to start a deploy!
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/deploy.sh
 
-# $1 deploy mode: fast or empty
+# $1: deploy mode (fast or <empty>)
 
 SCRIPT_NAME=deploy
 source $(dirname $(readlink -f $0))/script_begin.sh
 
 sudo bash ${SCRIPT_DIR}deploy_run.sh $1 2>&1 | sudo tee ${PROJECT_DIR}var/log/deploy.sh.log
-
