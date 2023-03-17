64041cfab66db70c130ef21a4f589b732fe92ea2
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:24:46 +0100

    Update cache-clear.sh

--- my-app-template/scripts/cache-clear.sh
+++ my-app-template/scripts/cache-clear.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-## Framework-based clear-cache by WEBSTACKUP
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/clear-cache.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
 wsuSourceFrameworkScript cache-clear "$@"
 source "${SCRIPT_DIR}script_end.sh"
-
