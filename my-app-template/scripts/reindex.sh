8d7c3208399bc9de825510ef108615d20150bbd1
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:39:18 +0100

    Update reindex.sh

--- my-app-template/scripts/reindex.sh
+++ my-app-template/scripts/reindex.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## reindex placeholder
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/reindex.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
