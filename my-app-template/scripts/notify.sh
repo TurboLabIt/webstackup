f5bb14882d449b8f56c4e8c4a6d63c347c45017b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:38:23 +0100

    Update notify.sh

--- my-app-template/scripts/notify.sh
+++ my-app-template/scripts/notify.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## team notifier
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/notify.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
