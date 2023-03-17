e98025c5af9bfbc844a2cb9cc9e3ebbd95569edd
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:36:00 +0100

    Update db-dump.sh

--- my-app-template/scripts/db-dump.sh
+++ my-app-template/scripts/db-dump.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## Framework-based database dump by WEBSTACKUP
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/db-dump.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
