aac58683077c95b63d460f649753757227515a94
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:36:10 +0100

    Update db-restore.sh

--- my-app-template/scripts/db-restore.sh
+++ my-app-template/scripts/db-restore.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## Framework-based database dump loader by WEBSTACKUP
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/db-load.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
