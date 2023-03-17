e8d191c7ed81c1f84f8bc774b5750f178ba188c7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:40:26 +0100

    Update wordpress-install.sh

--- my-app-template/scripts/wordpress-install.sh
+++ my-app-template/scripts/wordpress-install.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## WordPress installer
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/wordpress-install.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
