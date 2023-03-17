b158e22ad2b117abc9f78d184a51a376c230023c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:37:05 +0100

    Update deploy_moment_030.sh

--- my-app-template/scripts/deploy_moment_030.sh
+++ my-app-template/scripts/deploy_moment_030.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## This script is sourced by `deploy_run.sh` See: https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/deploy_run.sh
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/deploy_moment_030.sh
 
 #fxTitle "📦 Setting up composer credentials..."
