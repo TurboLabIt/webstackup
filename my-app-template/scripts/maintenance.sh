a6d93c823ea6ea8a099fdd5f0a800afb33b7e20e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:37:58 +0100

    Update maintenance.sh

--- my-app-template/scripts/maintenance.sh
+++ my-app-template/scripts/maintenance.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## Framework-based maintenance handler by WEBSTACKUP
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/maintenance.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
