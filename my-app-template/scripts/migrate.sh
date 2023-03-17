8621f305a5fd1321a170f4b59b6755be99b6d3ac
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-03-17 11:38:10 +0100

    Update migrate.sh

--- my-app-template/scripts/migrate.sh
+++ my-app-template/scripts/migrate.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-## framework migrations
-#
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/migrate.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
