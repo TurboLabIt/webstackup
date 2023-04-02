4c6715699a85786ccd2f5f0d5a340875b2b8bd81
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-04-02 17:58:09 +0200

    Update script_begin.sh

--- my-app-template/scripts/script_begin.sh
+++ my-app-template/scripts/script_begin.sh
@@ -2,9 +2,10 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/script_begin.sh
 
 APP_NAME="my-app"
-USERS_TEMPLATE_PATH="${PRIVGEN_DIR}operations/accounts/my-company/"
 PROJECT_FRAMEWORK=my-app-framework
 MAGENTO_STATIC_CONTENT_DEPLOY="MyCompany/my-app en_US it_IT fr_FR de_DE en_GB es_ES"
 
 ## https://github.com/TurboLabIt/webstackup/blob/master/script/filesystem/script_begin_start.sh
 source "/usr/local/turbolab.it/webstackup/script/filesystem/script_begin_start.sh" 
+
+USERS_TEMPLATE_PATH="${PRIVGEN_DIR}operations/accounts/my-company/"
