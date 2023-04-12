795d9727be0a3b23cca84a69c4f6db3191466cab
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-04-12 11:03:51 +0200

    Create remove.sh

---
+++ script/mysql/remove.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+### AUTOMATIC MYSQL UNINSTALLER BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/mysql/remove.sh
+#
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/mysql/remove.sh?$(date +%s) | sudo bash
+
+
+## bash-fx
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "🧹 MySQL Remover"
+rootCheck
+
+
+fxTitle "Removing MySQL..."
+service mysql stop
+apt purge --auto-remove mysql* -y
+
+
+fxTitle "Removing data..."
+rm -rf /var/lib/mysql
+
+
+fxTitle "Removing config..."
+rm -rf /etc/mysql
+
+
+fxTitle "Removing repository-related files..."
+rm -rf /etc/apt/trusted.gpg.d/*mysql*
+rm -rf /etc/apt/sources.list.d/*mysql*
+
+
+fxTitle "Removing webstackup maintenance script..."
+rm -rf /etc/cron.d/webstackup-mysql
+
+
+fxEndFooter
