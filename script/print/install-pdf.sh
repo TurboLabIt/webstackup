a9313dd1f17b2a8e9ca2cc1e04a81b67b6095923
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-04-13 13:01:53 +0200

    Create install-pdf.sh

---
+++ script/print/install-pdf.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+### AUTOMATIC PDF SUPPORT INSTALLER BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/print/pdf-install.sh
+#
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/print/pdf-install.sh?$(date +%s) | sudo bash
+#
+
+## bash-fx
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "💿 PDF support installer"
+rootCheck
+
+fxTitle "Installing GhostScript 👻..."
+apt install ghostscript -y
+
+fxEndFooter
