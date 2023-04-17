602e6af79e9612e8473965bd838943346bc9bbee
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-04-17 18:11:14 +0200

    Delete pimcore-flush-data-storage.sh

--- my-app-template/scripts/pimcore-flush-data-storage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/pimcore-flush-data-storage.sh
-
-source $(dirname $(readlink -f $0))/script_begin.sh
-source ${WEBSTACKUP_SCRIPT_DIR}frameworks/pimcore/flush-data-storage.sh
-source "${SCRIPT_DIR}/script_end.sh"
