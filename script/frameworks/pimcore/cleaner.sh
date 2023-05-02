76a53d1761170955baffe095628c1a06f7dc8e81
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-05-02 15:53:29 +0200

    Update cleaner.sh

--- script/frameworks/pimcore/cleaner.sh
+++ script/frameworks/pimcore/cleaner.sh
@@ -6,9 +6,9 @@ lockCheck ${LOCKFILE}
 source /etc/turbolab.it/mysql-${APP_NAME}.conf
 
 fxTitle "Flush versions/asset..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from `${MYSQL_DB_NAME}`.versions where ctype='asset'";
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='asset'";
 rm -rf ${PROJECT_DIR}var/versions/asset/*
 
 fxTitle "Flush recyclebin..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "TRUNCATE TABLE `${MYSQL_DB_NAME}`.recyclebin";
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "TRUNCATE TABLE \`${MYSQL_DB_NAME}\`.recyclebin";
 rm -rf ${PROJECT_DIR}var/recyclebin/*
