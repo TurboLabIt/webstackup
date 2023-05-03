bad88d57cbbc33ef8067fbd60f6f3f917e2ec6e7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-05-03 16:02:02 +0200

    Update cleaner.sh

--- script/frameworks/pimcore/cleaner.sh
+++ script/frameworks/pimcore/cleaner.sh
@@ -9,6 +9,14 @@ fxTitle "Flush versions/asset..."
 mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='asset'";
 rm -rf ${PROJECT_DIR}var/versions/asset/*
 
+fxTitle "Flush versions/document..."
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='document'";
+rm -rf ${PROJECT_DIR}var/versions/document/*
+
+fxTitle "Flush versions/object..."
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='object'";
+rm -rf ${PROJECT_DIR}var/versions/object/*
+
 fxTitle "Flush recyclebin..."
 mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "TRUNCATE TABLE \`${MYSQL_DB_NAME}\`.recyclebin";
 rm -rf ${PROJECT_DIR}var/recyclebin/*
