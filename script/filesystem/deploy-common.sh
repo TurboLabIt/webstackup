a5d6f7623653b12502a64494d3bf13bf77b161e6
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-05-18 18:27:50 +0200

    Update deploy-common.sh

--- script/filesystem/deploy-common.sh
+++ script/filesystem/deploy-common.sh
@@ -73,8 +73,6 @@ echo "Hash: $DEPLOY_SCRIPT_PREPULL_HASH"
 
 ## pulling and merging
 printTitle "⏬ Git pulling..."
-sudo -u $(logname) -H git config --global --add safe.directory "${PROJECT_DIR}"
-sudo -u ${EXPECTED_USER} -H git config --global --add safe.directory "${PROJECT_DIR}"
 sudo -u ${EXPECTED_USER} -H git -C "${PROJECT_DIR}" reset --hard
 sudo -u ${EXPECTED_USER} -H git -C "${PROJECT_DIR}" pull
 sudo -u ${EXPECTED_USER} -H git -C "${PROJECT_DIR}" gc --aggressive
