66bdbd7c931ce3b03f09c5dcfc3736baee363850
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-06-01 15:04:18 +0200

    Update cache-clear.sh

--- script/frameworks/pimcore/cache-clear.sh
+++ script/frameworks/pimcore/cache-clear.sh
@@ -22,8 +22,11 @@ if [ -z "${FAST_CACHE_CLEAR}" ]; then
   fxTitle "🧹 Removing Pimcore cache folder..."
   sudo rm -rf "${PROJECT_DIR}var/cache"
   
-  fxTitle "🔎 --create-or-update-index-structure"
-  wsuSymfony console ecommerce:indexservice:bootstrap --create-or-update-index-structure
+  if [ -d "${PROJECT_DIR}vendor/pimcore/pimcore/bundles/EcommerceFrameworkBundle" ]; then
+  
+    fxTitle "🔎 --create-or-update-index-structure"
+    wsuSymfony console ecommerce:indexservice:bootstrap --create-or-update-index-structure
+  fi
   
   fxTitle "🐧 Setting permissions..."
   ## https://pimcore.com/docs/pimcore/current/Development_Documentation/Installation_and_Upgrade/System_Setup_and_Hosting/File_Permissions.html
