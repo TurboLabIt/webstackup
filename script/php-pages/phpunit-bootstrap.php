cc09fdfc6c7825957b591e8182f0a213364c25ba
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-09-17 23:04:45 +0200

    Update phpunit-bootstrap.php

--- script/php-pages/phpunit-bootstrap.php
+++ script/php-pages/phpunit-bootstrap.php
@@ -3,6 +3,28 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require 'vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv('.env');
+$arrEnvFiles = [
+    '.env',
+    '.env.local'
+];
+
+if( file_exists('.env.local.php') ) {
+    $arrDumpedEnv = include '.env.local.php';
+}
+
+$realEnv = $arrDumpedEnv["APP_ENV"] ?? 'dev';
+
+$arrEnvFiles =
+    array_merge($arrEnvFiles, [
+        ".env.$realEnv",
+        ".env.$realEnv.local"
+    ]);
+
+$dotEnvLoaderExists = method_exists(Dotenv::class, 'bootEnv');
+if($dotEnvLoaderExists) {
+    foreach($arrEnvFiles as $envFile) {
+        if( file_exists($envFile) ) {
+            (new Dotenv())->bootEnv($envFile);
+        }
+    }
 }
