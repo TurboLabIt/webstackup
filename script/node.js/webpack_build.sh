0724320f11749820db0f87d34d67e540c81ce811
fabioimperiale <fabio.imperiale@boraso.com>
2023-11-09 14:50:55 +0100

    Create webpack_build.sh
    Add yarn build script

---
+++ script/node.js/webpack_build.sh
@@ -0,0 +1,14 @@
+fxHeader "🏗 ${APP_NAME} watch"
+
+fxTitle "🤹 Setting node.js version..."
+sudo n 20
+sudo -u $EXPECTED_USER -H node --version
+
+fxTitle "💿 yarn install..."
+sudo -u $EXPECTED_USER -H yarn install
+
+fxTitle "📦 Webpack..."
+sudo -u $EXPECTED_USER -H yarn webpack
+
+fxTitle "👀 Watch..."
+sudo -u $EXPECTED_USER -H yarn build
