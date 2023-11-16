658db4deca496ea734bf8e4af8733c8888873dcb
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-11-16 10:16:24 +0100

    Update cron.sh

--- script/frameworks/symfony/cron.sh
+++ script/frameworks/symfony/cron.sh
@@ -19,4 +19,5 @@ fxTitle "Stopping workers..."
 bash ${SCRIPT_DIR}cli.sh messenger:stop-workers
 
 fxTitle "Starting workers..."
-bash ${SCRIPT_DIR}cli.sh messenger:consume async -vv --limit 50 &
+bash ${SCRIPT_DIR}cli.sh messenger:consume async -vv --limit 50
+# & doesn't work :-(
