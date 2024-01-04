71491a0f111f80bb4f9a964e7e8dea24ca0261a7
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-01-04 09:48:35 +0100

    Update db-restore-after.sh

--- script/frameworks/db-restore-after.sh
+++ script/frameworks/db-restore-after.sh
@@ -1,1 +1,9 @@
-bash "${SCRIPT_DIR}cache-clear.sh"
+if [ "${SKIP_POST_RESTORE_QUERY}" != "1" ]; then
+
+  bash "${SCRIPT_DIR}cache-clear.sh"
+
+else
+
+  fxTitle "⚙️ Running db-post-restore SQL queries..."
+  fxInfo "SKIP_POST_RESTORE_QUERY is set, skipping"
+fi
