36b0ed9fb40f5c93a6ff6dfc4c39946b6e0c93e3
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-13 14:33:55 +0100

    Update cleaner.sh

--- script/frameworks/pimcore/cleaner.sh
+++ script/frameworks/pimcore/cleaner.sh
@@ -6,17 +6,17 @@ lockCheck ${LOCKFILE}
 source /etc/turbolab.it/mysql-${APP_NAME}.conf
 
 fxTitle "Flush versions/asset..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='asset'";
-rm -rf ${PROJECT_DIR}var/versions/asset/*
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='asset'" && \
+  rm -rf ${PROJECT_DIR}var/versions/asset/*
 
 fxTitle "Flush versions/document..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='document'";
-rm -rf ${PROJECT_DIR}var/versions/document/*
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='document'" && \
+  rm -rf ${PROJECT_DIR}var/versions/document/*
 
 fxTitle "Flush versions/object..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='object'";
-rm -rf ${PROJECT_DIR}var/versions/object/*
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DELETE from \`${MYSQL_DB_NAME}\`.versions where ctype='object'" && \
+  rm -rf ${PROJECT_DIR}var/versions/object/*
 
 fxTitle "Flush recyclebin..."
-mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "TRUNCATE TABLE \`${MYSQL_DB_NAME}\`.recyclebin";
-rm -rf ${PROJECT_DIR}var/recyclebin/*
+mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "TRUNCATE TABLE \`${MYSQL_DB_NAME}\`.recyclebin" && \
+  rm -rf ${PROJECT_DIR}var/recyclebin/*
