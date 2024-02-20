6034fe0203b12f86a8e27c505a7d12e9cb148d36
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-20 22:29:20 +0100

    Update phpbb-cli.sh

--- my-app-template/scripts/phpbb-cli.sh
+++ my-app-template/scripts/phpbb-cli.sh
@@ -2,5 +2,5 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/phpbb-cli.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
-source "${WEBSTACKUP_SCRIPT_DIR}frameworks/phpbb-cli.sh"
+source "${WEBSTACKUP_SCRIPT_DIR}frameworks/phpbb/cli.sh"
 source "${SCRIPT_DIR}/script_end.sh"
