5142db99591dc7d455810d8d9c7fb985f1fc314f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 21:06:31 +0100

    Update SymfonyWebTestCase.php

--- script/php-pages/SymfonyWebTestCase.php
+++ script/php-pages/SymfonyWebTestCase.php
@@ -1,10 +1,36 @@
 <?php
-namespace MyVendorName\ThisPackageNameBundle\tests;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+/**
+ * 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/script/php-pages/SymfonyWebTestCase.php
+ *
+ * 📚 Usage example (customize with your own):
+ *
+ * - MyVendorName   ➡ TurboLabIt
+ * - MyPackageName  ➡ BaseCommand
+ *
+ * 💡 "Replace all" the above and you're done!
+ *
+ * Expected result:
+ *
+ * - filename           ➡ <Whatever>Test.php
+ * - namespace          ➡ TurboLabIt\BaseCommandBundle\tests
+ * - class              ➡ <Whatever>Test
+ */
+namespace MyVendorName\MyPackageNameBundle\tests;
 
 
 class BundleTest extends WebTestCase
 {
+    protected static function somethingProvider()
+    {
+        yield ['/aaa', '/bbb', '/ccc'];
+    }
 
+
+    /**
+     * @dataProvider somethingProvider
+     */
+    public function testSomething(string $value)
+    {
+        $this->assertNotEmpty($value);
+    }
 }
