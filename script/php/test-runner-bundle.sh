cecda94de03c8f31f57a526cf695c713ea83c045
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 16:11:05 +0100

    Update test-runner-bundle.sh

--- script/php/test-runner-bundle.sh
+++ script/php/test-runner-bundle.sh
@@ -7,3 +7,16 @@
 #   curl -Lo scripts/test-runner-bundle.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/my-app-template/scripts/test-runner-bundle.sh && sudo chmod u=rwx,go=rx scripts/*.sh
 #
 # 1. You should now git commit your copy
+
+echo ""
+SCRIPT_NAME=test-runner-bundle
+
+## https://github.com/TurboLabIt/bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "🧪 Test Runner"
