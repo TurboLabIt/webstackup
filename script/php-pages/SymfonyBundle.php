c1ce73bb5cae3e92743091692bc0ca5ad374142b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 18:56:53 +0100

    Update SymfonyBundle.php

--- script/php-pages/SymfonyBundle.php
+++ script/php-pages/SymfonyBundle.php
@@ -1,11 +1,31 @@
 <?php
-namespace Acme\BlogBundle;
+/**
+ * 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/script/php-pages/SymfonyBundle.php
+ *
+ * 📚 Usage example (customize with your own):
+ *
+ * - MyVendorName   ➡ TurboLabIt
+ * - MyPackageName  ➡ BaseCommand
+ *
+ * 💡 "Replace all" the above and you're done!
+ *
+ * Results:
+ *
+ * - filename       ➡ TurboLabItBaseCommandBundle.php
+ * - namespace      ➡ TurboLabIt\BaseCommandBundle
+ * - class          ➡ TurboLabItBaseCommandBundle
+ *
+ * ✅ You're done && ready to go here!
+ *
+ * 📚 Resources:
+ *
+ * - https://symfony.com/doc/current/bundles.html
+ * - https://symfonycasts.com/screencast/symfony-bundle/bundle-directory
+ */
+namespace MyVendorName\MyPackageNameBundle;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 
-/**
-
 class MyVendorNameMyPackageNameBundle extends AbstractBundle
-{
-}
+{}
