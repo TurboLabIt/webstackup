5a0a96e52ce2812570aa3e9bc915649fb9db9c45
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 16:07:26 +0100

    Create test-runner-bundle.sh

---
+++ script/php/test-runner-bundle.sh
@@ -0,0 +1,9 @@
+## Run phpunit tests of your bundle by WEBSTACKUP
+# https://github.com/TurboLabIt/webstackup/blob/master/script/php/test-runner-bundle.sh
+#
+# How to:
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/test-runner-bundle.sh https://raw.githubusercontent.com/TurboLabIt/webstackup/master/my-app-template/scripts/test-runner-bundle.sh && sudo chmod u=rwx,go=rx scripts/*.sh
+#
+# 1. You should now git commit your copy
