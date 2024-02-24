d325e151e1e33d07605df79060e45bd82e6c773a
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 16:43:09 +0100

    Create test-runner-bundle.sh

---
+++ my-app-template/scripts/test-runner-bundle.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/script/php/test-runner-bundle.sh
+
+clear
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+source <(curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/php/test-runner-bundle.sh)
