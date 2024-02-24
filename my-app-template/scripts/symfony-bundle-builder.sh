6b3959b2d92001f471b755d563672f9ecba5e6eb
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-02-24 19:05:47 +0100

    Update symfony-bundle-builder.sh

--- my-app-template/scripts/symfony-bundle-builder.sh
+++ my-app-template/scripts/symfony-bundle-builder.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/script/php/test-runner-bundle.sh
+# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/symfony-bundle-builder.sh
+# 📚 Usage guide: https://github.com/TurboLabIt/webstackup/blob/master/script/php/symfony-bundle-builder.sh
 
 clear
 if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+## https://github.com/TurboLabIt/webstackup/blob/master/script/php/symfony-bundle-builder.sh
 source <(curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/php/symfony-bundle-builder.sh)
