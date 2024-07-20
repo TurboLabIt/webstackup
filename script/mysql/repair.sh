857c46a609173439fd458f266dd070ab9d35b47f
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-07-20 12:43:17 +0200

    Create repair.sh

---
+++ script/mysql/repair.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+### AUTOMATIC MYSQL CHECK AND REPAIR BY WEBSTACK.UP
+#
+
+## THIS SCRIPT WAS MADE IN A RUSH AND NEEDS SOME LOVE
+
+source /etc/turbolab.it/mysql.conf
+
+mysqlcheck -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h "${MYSQL_HOST}" --all-databases --auto-repair
