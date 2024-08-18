c6445a373f34261df329560f82d586e9a827684e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-08-18 11:35:48 +0200

    Update webpack_watch.sh
    NODEJS_VER version check

--- script/node.js/webpack_watch.sh
+++ script/node.js/webpack_watch.sh
@@ -1,14 +1,19 @@
 fxHeader "🏗 ${APP_NAME} watch"
 
-fxTitle "🤹 Setting node.js version..."
-sudo n 20
+if [ ! -z "${NODEJS_VER}" ]; then
+
+  fxTitle "🤹 Setting node.js version..."
+  sudo n 20
+fi
+
+fxTitle "🤹 node.js version in use"
 sudo -u $EXPECTED_USER -H node --version
 
 fxTitle "🔄 Setting Yarn version to stable..."
 sudo -u $EXPECTED_USER -H yarn set version stable
 
 fxTitle "💿 yarn install..."
-sudo -u $EXPECTED_USER -H yarn install
+echo "y" | sudo -u $EXPECTED_USER -H yarn install
 
 fxTitle "📦 Webpack..."
 sudo -u $EXPECTED_USER -H yarn webpack
