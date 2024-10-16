b849aaf38a67c3775af74c6840f346b068891dd5
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-10-16 17:27:32 +0200

    Update script_begin.sh

--- script/frameworks/magento/script_begin.sh
+++ script/frameworks/magento/script_begin.sh
@@ -5,7 +5,7 @@ MAGENTO_DIR=${PROJECT_DIR}shop/
 WEBROOT_DIR=${MAGENTO_DIR}pub/
 MAGENTO_MODULE_DISABLE=" \
   Magento_AdminAdobeImsTwoFactorAuth \
-  Magento_TwoFactorAuth Magento_Csp Mageplaza_Core Magento_LoginAsCustomerGraphQl Magento_LoginAsCustomerAssistance \
+  Magento_TwoFactorAuth Mageplaza_Core Magento_LoginAsCustomerGraphQl Magento_LoginAsCustomerAssistance \
   Magento_StoreGraphQl Magento_CatalogRuleGraphQl Magento_CheckoutAgreementsGraphQl Magento_CmsUrlRewriteGraphQl \
   Magento_CompareListGraphQl Magento_DirectoryGraphQl Magento_DownloadableGraphQl \
   Magento_CustomerDownloadableGraphQl Magento_CatalogCustomerGraphQl Magento_BundleGraphQl Magento_GiftMessageGraphQl \
