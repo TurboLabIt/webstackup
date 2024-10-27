f555c4bbe73e861919d331b12c7e69c91e0e5d41
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-10-27 11:43:14 +0100

    !!!WiP!!! Varnish installer

--- script/varnish/install.sh
+++ script/varnish/install.sh
@@ -2,7 +2,7 @@
 ### AUTOMATIC VARNISH INSTALLER BY WEBSTACK.UP
 # https://github.com/TurboLabIt/webstackup/tree/master/script/varnish/install.sh
 #
-# sudo apt update && sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/varnish/install.sh | sudo bash
+# sudo apt update && sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/varnish/install.sh | sudo VARNISH_VER=7.6 bash
 #
 # Based on: https://turbolab.it/4221 | https://www.varnish-software.com/developers/tutorials/installing-varnish-ubuntu/
 
@@ -37,13 +37,13 @@ fxTitle "Installing additional utilities..."
 apt install software-properties-common openssl zip unzip nano -y
 
 fxTitle "Import the official varnish signing key..."
-curl -L https://packagecloud.io/varnishcache/varnish60lts/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/varnish-archive-keyring.gpg >/dev/null
+curl -L https://packagecloud.io/varnishcache/varnish${VARNISH_VER//./}/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/varnish-archive-keyring.gpg >/dev/null
 
 
 fxTitle "Creating the apt source file..."
 . /etc/os-release
 echo "deb [signed-by=/usr/share/keyrings/varnish-archive-keyring.gpg] \
-https://packagecloud.io/varnishcache/varnish60lts/$ID/ $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/varnish.list
+https://packagecloud.io/varnishcache/${VARNISH_VER//./}/$ID/ $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/varnish.list
 
 fxTitle "Set up repository pinning to prefer our packages over distribution-provided ones..."
 echo -e "Package: varnish varnish-*\nPin: release o=packagecloud.io/varnishcache/*\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99varnish
