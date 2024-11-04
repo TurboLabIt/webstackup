83f4e5e3e08bfef2d7e4a02679f51a3b843bad05
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2024-11-04 15:49:30 +0100

    Update cache-clear.sh

--- script/frameworks/symfony/cache-clear.sh
+++ script/frameworks/symfony/cache-clear.sh
@@ -21,7 +21,6 @@ if [ -z "${PROJECT_DIR}" ] || [ ! -d "${PROJECT_DIR}" ]; then
 fi
 
 if [ "$1" = "fast" ]; then
-
   FAST_CACHE_CLEAR=1
 fi
 
@@ -32,7 +31,6 @@ cd "$PROJECT_DIR"
 fxTitle "Setting var/log permissions..."
 sudo chmod ugo= "${PROJECT_DIR}var/log" -R
 sudo chmod ugo=rwX "${PROJECT_DIR}var/log" -R
-
 
 fxTitle "Temporary open permissions on cache..."
 sudo chmod ugo=rwx "${PROJECT_DIR}var/cache" -R
@@ -67,22 +65,20 @@ elif [ -z "${FAST_CACHE_CLEAR}" ]; then
 fi
 
 
-if [ -z "${FAST_CACHE_CLEAR}" ]; then
+if [ -z "${FAST_CACHE_CLEAR}" ] && [ "${APP_ENV}" != "dev" ]; then
 
-  #fxTitle "⚙️ Stopping services.."
-  #sudo nginx -t && sudo service nginx stop && sudo service ${PHP_FPM} stop
-
-  ## https://github.com/symfony/monolog-bundle/issues/288
-  fxTitle "🧹 Removing the Symfony cache folder..."
-  sudo rm -rf "${PROJECT_DIR}var/cache"
-
-  fxTitle "☀ Creating the Symfony cache folder anew..."
-  sudo mkdir -p "${PROJECT_DIR}var/cache"
-
-else
-
-  fxTitle "📐 Symfony cache folder NOT removed (fast mode)"
+  fxTitle "⚙️ Stopping services..."
+  sudo nginx -t && sudo service nginx stop
+  sudo service ${PHP_FPM} restart
 fi
+
+
+## https://github.com/symfony/monolog-bundle/issues/288
+fxTitle "🧹 Removing the Symfony cache folder..."
+sudo rm -rf "${PROJECT_DIR}var/cache"
+
+fxTitle "☀ Creating the Symfony cache folder anew..."
+sudo mkdir -p "${PROJECT_DIR}var/cache"
 
 
 fxTitle "👮 Setting final permissions on var/cache..."
@@ -95,24 +91,19 @@ sudo chmod g+s "${PROJECT_DIR}var/cache"
 fxTitle "🌊 Symfony cache:clear..."
 sudo rm -rf /tmp/symfony-cache
 sudo -u www-data -H XDEBUG_MODE=off symfony console cache:clear --no-optional-warmers
+
+
+if [ -z "${FAST_CACHE_CLEAR}" ] && [ "${APP_ENV}" != "dev" ]; then
+
+  fxTitle "⚙️ Restarting services..."
+  sudo service ${PHP_FPM} restart
+  sudo nginx -t && sudo service nginx start
+fi
+
+
+fxTitle "🔥 Cache warmup..."
 sudo -u www-data -H XDEBUG_MODE=off symfony console cache:warmup 
 #&> "${PROJECT_DIR}var/log/cache-warmer.log" &
-
-fxTitle "Final status..."
-ls -ld "${PROJECT_DIR}var"
-ls -la "${PROJECT_DIR}var/cache"
-
-
-if [ -z "${FAST_CACHE_CLEAR}" ]; then
-
-  fxTitle "⚙️ Restarting services.."
-  #sudo nginx -t && sudo service ${PHP_FPM} restart && sudo service nginx restart
-
-else
-
-  fxTitle "🌊 PHP OPcache clear..."
-  wsuOpcacheClear
-fi
 
 
 if [ "$APP_ENV" = "dev" ]; then
@@ -122,3 +113,8 @@ if [ "$APP_ENV" = "dev" ]; then
   sudo chmod ugo= "${PROJECT_DIR}" -R
   sudo chmod ugo=rwx "${PROJECT_DIR}" -R
 fi
+
+
+fxTitle "Final status..."
+ls -ld "${PROJECT_DIR}var"
+ls -la "${PROJECT_DIR}var/cache"
