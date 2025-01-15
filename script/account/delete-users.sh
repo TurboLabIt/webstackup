1853a81cedb5f9e4dcdcd12c1f6da01eb6cb7100
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-01-15 11:29:36 +0100

    Create delete-users.sh

---
+++ script/account/delete-users.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+### LINUX USERS ACCOUNT REMOVER BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/account/delete-users.sh
+#
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/account/delete-users.sh | sudo bash
+
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "🗑️ Remove Linux users"
+rootCheck
+
+
+## Read usernames-as-arguments from the command-line
+USERS_TO_REMOVE=("$@")
+for NAME in "${USERS_TO_REMOVE[@]}"; do
+
+  if ! id "$NAME" &>/dev/null; then
+    continue
+  fi
+
+  sudo killall -u "$NAME"
+  sudo deluser --remove-home "$NAME"
+  if [ $? -ne 0 ]; then
+    fxWarning "error deleting Linux user $NAME"
+  fi
+done
+
+
+fxEndFooter
