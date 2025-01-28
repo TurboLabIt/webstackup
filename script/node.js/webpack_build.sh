c1de0f88789400a4df5be288a5f913217851bdce
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-01-28 10:18:32 +0100

    Update webpack_build.sh

--- script/node.js/webpack_build.sh
+++ script/node.js/webpack_build.sh
@@ -1,21 +1,40 @@
 fxHeader "🏗 ${APP_NAME} build"
 
+
 if [ ! -z "${NODEJS_VER}" ]; then
 
   fxTitle "🤹 Setting node.js version..."
   sudo n 20
 fi
 
+
 fxTitle "🤹 node.js version in use"
 sudo -u $EXPECTED_USER -H node --version
 
+
 fxTitle "💿 yarn install..."
 echo "y" | sudo -u $EXPECTED_USER -H yarn install
+
 
 fxTitle "👮 Fixing permissions..."
 sudo chmod ug+x node_modules/.bin -R
 sudo chmod ug+x node_modules/webpack* -R
 sudo chmod ug+x node_modules/@webpack-cli -R
 
-fxTitle "👀 build..."
-sudo -u $EXPECTED_USER -H yarn dist
+
+fxTitle "🔨 build with yarn..."
+if grep -q '"dist":' package.json; then
+
+  fxInfo "yarn dist"
+  sudo -u $EXPECTED_USER -H yarn dist
+
+elif grep -q '"build":' package.json; then
+
+  fxInfo "build"
+  sudo -u $EXPECTED_USER -H yarn build
+  
+else
+
+  fxInfo "webpack"
+  sudo -u $EXPECTED_USER -H yarn webpack
+fi
