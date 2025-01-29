217d1d24c08a6e4891747850b42d54571b02842d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-01-29 08:29:22 +0100

    Update install.sh

--- script/varnish/install.sh
+++ script/varnish/install.sh
@@ -73,6 +73,7 @@ apt install varnish -y
 
 
 fxTitle "Installing the customized default.vcl..."
+rm -f /etc/varnish/default.vcl
 cp ${WSU_DIR}my-app-template/config/custom/varnish.vcl /etc/varnish/default.vcl
 sed -i '1,/^# Based on/d' /etc/varnish/default.vcl
 
