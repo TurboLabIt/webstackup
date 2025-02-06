daf00e083a444706c8e61c7c135b44225ff26474
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-02-06 12:01:45 +0100

    Update db-restore-after.sh

--- script/frameworks/db-restore-after.sh
+++ script/frameworks/db-restore-after.sh
@@ -1,9 +1,25 @@
-if [ "${SKIP_POST_RESTORE_QUERY}" != "1" ]; then
+fxTitle "⚙️ Running post-restore..."
 
-  bash "${SCRIPT_DIR}cache-clear.sh"
+if [ "${SKIP_POST_RESTORE_QUERY}" == "1" ]; then
+
+  fxInfo "SKIP_POST_RESTORE_QUERY is set, skipping 🦘"
 
 else
 
-  fxTitle "⚙️ Running db-post-restore SQL queries..."
-  fxInfo "SKIP_POST_RESTORE_QUERY is set, skipping"
+  if [ "${APP_ENV}" == "master" ] && [ -f "${PROJECT_DIR}config/custom/prod/db-post-restore.sql" ]; then
+
+    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h "${MYSQL_HOST}" < "${PROJECT_DIR}config/custom/prod/db-post-restore.sql"
+
+  elif [[ "${APP_ENV}" == "staging" || "${APP_ENV}" == "dev" ]] && [ -f "${PROJECT_DIR}config/custom/staging/db-post-restore.sql" ]; then
+
+    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h "${MYSQL_HOST}" < "${PROJECT_DIR}config/custom/staging/db-post-restore.sql"
+
+  fi
+
+
+  if [ "${APP_ENV}" == "dev" ] && [ -f "${PROJECT_DIR}config/custom/dev/db-post-restore.sql" ]; then
+    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h "${MYSQL_HOST}" < "${PROJECT_DIR}config/custom/dev/db-post-restore.sql"
+  fi
+
+  bash "${SCRIPT_DIR}cache-clear.sh"
 fi
