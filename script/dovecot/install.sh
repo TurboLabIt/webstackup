30997733d7c644f7f4e76582bba3a0721be1a707
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-02-16 08:33:57 +0100

    Update install.sh

--- script/dovecot/install.sh
+++ script/dovecot/install.sh
@@ -90,7 +90,6 @@ fi
 fxTitle "Wiring together Postfix and Dovecot..."
 echo "" >>  /etc/postfix/main.cf
 echo "" >>  /etc/postfix/main.cf
-echo "" >>  /etc/postfix/main.cf
 cat "${WEBSTACKUP_CONFIG_DIR}postfix/dovecot-integration-incoming.conf" | sed "s|my-app.com|${POSTFIX_MAIL_NAME}|g" >> /etc/postfix/main.cf
 echo "" >>  /etc/postfix/main.cf
 echo "" >>  /etc/postfix/main.cf
