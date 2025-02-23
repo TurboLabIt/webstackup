a019dedbe7253fde577d2eedcd065da9107ec40e
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-02-23 17:56:14 +0100

    Update certificate-renewal-action.sh

--- script/https/certificate-renewal-action.sh
+++ script/https/certificate-renewal-action.sh
@@ -1,1 +1,22 @@
+if [ ! -z "$(command -v nginx)" ]; then
+  echo "Reloading NGINX...."
+  sudo nginx -t && sudo service nginx reload
+fi
 
+
+if [ ! -z "$(command -v apache2)" ]; then
+  echo "Reloading APACHE...."
+  sudo apachectl configtest && sudo service apache2 reload
+fi
+
+
+if [ ! -z "$(command -v postfix)" ]; then
+  echo "Reloading POSTFIX...."
+  sudo service postfix reload
+fi
+
+
+if [ ! -z "$(command -v dovecot)" ]; then
+  echo "Reloading DOVECOT...."
+  sudo service dovecot reload
+fi
