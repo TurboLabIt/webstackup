29c25eb1856569412b49c85978e7409d30ad8036
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-03-04 11:22:57 +0100

    Update db-restore.sh

--- script/frameworks/db-restore.sh
+++ script/frameworks/db-restore.sh
@@ -6,8 +6,17 @@ echo "🗄️ DB_DUMP_FILE_PATH:    ##${DB_DUMP_FILE_PATH}##"
 echo "📛 MYSQL_DB_NAME:        ##${MYSQL_DB_NAME}##"
 echo "⚙️ SKIP_POST_RESTORE_QUERY:  ##${SKIP_POST_RESTORE_QUERY}#"
 
-fxEnvNotProd
-showPHPVer
+
+fxTitle "Env check..."
+fxOK "$APP_ENV"
+if [ "$APP_ENV" == "prod" ]; then
+
+  fxWarning "PROD env detected! Do you really want to restore the db IN PROD?"
+  read -p ">> " -n 1 -r  < /dev/tty
+  if [[ ! "$REPLY" =~ ^[Yy1]$ ]]; then
+    return 0
+  fi
+fi
 
 cd "${PROJECT_DIR}backup/db-dumps"
 
