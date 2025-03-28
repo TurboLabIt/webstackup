95a1492dc47ca96f8cb414921e076df8242728d1
Zane <info@turbolab.it>
2025-03-28 23:15:26 +0100

    mysql client access via GUI

--- script/mysql/zzdb_run.sh
+++ script/mysql/zzdb_run.sh
@@ -4,17 +4,22 @@
 
 TITLE="MySQL management GUI"
 OPTIONS=(
-  1 "🐬  New database user access"
-  2 "🔧  MySQL maintenance"
-  3 "💨  MySQL Tuner"
-  4 "🤦  MySQL password reset"
+  1 "🚪  Login to MySQL"
+  2 "🐬  New database user access"
+  3 "🔧  MySQL maintenance"
+  4 "💨  MySQL Tuner"
+  5 "🤦  MySQL password reset"
 )
 
 source "/usr/local/turbolab.it/webstackup/script/base-gui.sh"
 
 case $CHOICE in
-  1)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/new.sh";;
-  2)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/maintenance.sh";;
-  3)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/mysqltuner.sh";;
-  4)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/password-reset.sh";;
+  1)
+    source /etc/turbolab.it/mysql.conf
+    mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}"
+    ;;
+  2)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/new.sh";;
+  3)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/maintenance.sh";;
+  4)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/mysqltuner.sh";;
+  5)bash "${WEBSTACKUP_SCRIPT_DIR}mysql/password-reset.sh";;
 esac
