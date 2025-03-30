a3f31d6c762140533aa0aeaf6808615c23506394
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-03-30 23:08:36 +0200

    Update zzmail_run.sh

--- script/mail/zzmail_run.sh
+++ script/mail/zzmail_run.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 ### EMAIL TOOLS GUI by WEBSTACK.UP
 # https://github.com/TurboLabIt/webstackup/tree/master/script/email/zzmail.sh
-clear
 
-source "/usr/local/turbolab.it/webstackup/script/base.sh"
 TITLE="Email server management GUI"
-fxHeader "$TITLE"
-rootCheck
-
-if [ -z "$(command -v dialog)" ]; then
-  apt install dialog -y -qq
-fi
-
 OPTIONS=(
   1 "📤  Send a test email"
   2 "📫  New mailbox"
@@ -21,16 +12,7 @@ OPTIONS=(
   6 "📧  DKIM a domain"
 )
 
-CHOICE=$(dialog --clear \
-  --backtitle "$BACKTITLE" \
-  --title "$TITLE" \
-  --menu "$MENU" \
-  $HEIGHT $WIDTH $CHOICE_HEIGHT \
-  "${OPTIONS[@]}" \
-  2>&1 >/dev/tty)
-
-clear
-
+source "/usr/local/turbolab.it/webstackup/script/base-gui.sh"
 
 case $CHOICE in
   1) bash ${WEBSTACKUP_SCRIPT_DIR}mail/send-test-email.sh;;
