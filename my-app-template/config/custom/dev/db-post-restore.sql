2ee6fbc6c397f6950751d77f47783f9afd681a46
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-04-01 15:30:22 +0200

    Update db-post-restore.sql

--- my-app-template/config/custom/dev/db-post-restore.sql
+++ my-app-template/config/custom/dev/db-post-restore.sql
@@ -7,13 +7,10 @@
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/dev/db-post-restore.sql
 
-## dev0
-#USE my-app_dev0
-#UPDATE `core_config_data` SET `value` = 'https://dev0-next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
-#UPDATE `wp_options` SET `option_value` = 'https://dev0-next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
+#USE my-app_dev0;
+#MAGENTO: UPDATE `core_config_data` SET `value` = 'https://dev0-next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
+#WORDPRESS: UPDATE `wp_options` SET `option_value` = 'https://dev0-next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
 
 
-## dev1
-#USE my-app_dev1
-#UPDATE `core_config_data` SET `value` = 'https://dev1-next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
-#UPDATE `wp_options` SET `option_value` = 'https://dev1-next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
+#USE my-app_dev1;
+#...
