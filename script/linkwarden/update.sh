60d483c7935674a1a5701e674a5e9ed70355be6d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-05-05 06:55:24 +0200

    Create update.sh

---
+++ script/linkwarden/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+### AUTOMATIC LINKWARDEN UPDATER BY WEBSTACK.UP
+# https://github.com/TurboLabIt/webstackup/tree/master/script/linkwarden/update.sh
+#
+# Run this via /etc/turbolab.it/zzupdate.conf : 
+# ADDITIONAL_UPDATE_SCRIPT=/usr/local/turbolab.it/webstackup/script/linkwarden/update.sh
+#
+
+LOCAL_INSTALL_SCRIPT=/usr/local/turbolab.it/webstackup/script/linkwarden/install.sh
+if [ -f "${LOCAL_INSTALL_SCRIPT}" ]; then
+
+  bash "${LOCAL_INSTALL_SCRIPT}"
+
+else
+
+  curl -Ls https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/linkwarden/install.sh
+fi
