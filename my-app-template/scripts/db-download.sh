b6aff6060f834007d202888012af5ce5632f2237
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-05-30 15:34:24 +0200

    Update db-download.sh

--- my-app-template/scripts/db-download.sh
+++ my-app-template/scripts/db-download.sh
@@ -3,11 +3,22 @@
 
 source $(dirname $(readlink -f $0))/script_begin.sh
 
-REMOTE_SERVER=my-app.com
+## download from...
+if [ "${1}" == "staging" ]; then
+
+  REMOTE_SERVER=next.my-app.com
+  REMOTE_APP_ENV=staging
+
+else
+
+  REMOTE_SERVER=my-app.com
+  REMOTE_APP_ENV=prod
+fi
+
 REMOTE_SSH_USERNAME=
 REMOTE_PROJECT_DIR=/var/www/my-app/
-REMOTE_APP_ENV=prod
 DISABLE_SSH_TEST=0
+
 
 wsuSourceFrameworkScript db-download
 
