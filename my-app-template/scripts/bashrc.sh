b1885cbab769b7739d9bd6560bfaeb985e0e94e7
Zane <info@turbolab.it>
2025-09-20 11:43:10 +0200

    bashrc.sh

---
+++ my-app-template/scripts/bashrc.sh
@@ -0,0 +1,25 @@
+## this should be sourced from each user .bashrc
+
+# background color per-host
+if [[ $- == *i* && -n "$SSH_CONNECTION" ]]; then
+
+    source "/usr/local/turbolab.it/bash-fx/scripts/colors.sh"
+
+    case "$(hostname -s)" in
+      my-app.com)
+        # very dark red
+        fxSetBackgroundColor "#F2F2F2" "#240000"
+        ;;
+      next.my-app.com)
+        # very dark orange
+        fxSetBackgroundColor "#F2F2F2" "#241600"
+        ;;
+    esac
+
+  # Ensure colors go back to default when the shell exits
+  trap fxResetBackgroundColor EXIT
+
+fi
+
+
+cd /var/www/my-app
