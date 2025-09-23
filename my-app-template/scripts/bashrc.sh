3feb48378b2c1a2a96f6162b72549984438256f0
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-09-23 16:01:12 +0200

    Update bashrc.sh

--- my-app-template/scripts/bashrc.sh
+++ my-app-template/scripts/bashrc.sh
@@ -17,28 +17,9 @@
 
 ## this should be sourced from each user .bashrc
 
-# background color per-host
-if [[ $- == *i* && -n "$SSH_CONNECTION" ]]; then
-
-    source "/usr/local/turbolab.it/bash-fx/scripts/colors.sh"
-
-    case "$(hostname -s)" in
-      my-app.com)
-        # very dark red
-        fxSetBackgroundColor "#F2F2F2" "#240000"
-        ;;
-      next.my-app.com)
-        # very dark orange
-        fxSetBackgroundColor "#F2F2F2" "#241600"
-        ;;
-    esac
-
-  # Ensure colors go back to default when the shell exits
-  trap fxResetBackgroundColor EXIT
-
-fi
+source "/usr/local/turbolab.it/bash-fx/scripts/colors.sh"
+fxSetBackgroundColorByHostAndEnv "my-app.com" "prod"
+fxSetBackgroundColorByHostAndEnv "next.my-app.com" "next"
+trap fxResetBackgroundColor EXIT
 
 cd /var/www/my-app
-
-## if the server hosts multiple projects, go with this 👇🏻 instead
-#zzcd
