47e5b5db406f8d018bfafb8c0ca32e51b72fa712
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2025-10-23 12:59:00 +0200

    Update crawl.sh

--- my-app-template/scripts/crawl.sh
+++ my-app-template/scripts/crawl.sh
@@ -2,5 +2,6 @@
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/crawl.sh
 
 source $(dirname $(readlink -f $0))/script_begin.sh
+## https://github.com/TurboLabIt/webstackup/blob/master/script/https/crawl.sh
 source ${WEBSTACKUP_SCRIPT_DIR}https/crawl.sh
 source "${SCRIPT_DIR}script_end.sh"
