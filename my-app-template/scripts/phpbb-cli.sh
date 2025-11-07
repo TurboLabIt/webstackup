7c0338ea7acde35b754d7babea7442b2c8de34bb
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2025-11-07 11:55:57 +0100

    remove phpBB-related scripts

--- my-app-template/scripts/phpbb-cli.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/phpbb-cli.sh
-
-source $(dirname $(readlink -f $0))/script_begin.sh
-source "${WEBSTACKUP_SCRIPT_DIR}frameworks/phpbb/cli.sh"
-source "${SCRIPT_DIR}/script_end.sh"
--- my-app-template/scripts/phpbb-upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/scripts/phpbb-upgrade.sh
-
-source $(dirname $(readlink -f $0))/script_begin.sh
-bash /usr/local/turbolab.it/phpbb-upgrader/phpbb-upgrader.sh my-app
-source "${SCRIPT_DIR}/script_end.sh"
