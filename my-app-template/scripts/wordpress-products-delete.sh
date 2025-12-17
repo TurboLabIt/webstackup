c40bad2176f0479399e244c64453b4826980854c
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2025-12-17 16:36:38 +0100

    wordpress products delete script

--- my-app-template/scripts/wordpress-products-delete.sh
+++ my-app-template/scripts/wordpress-products-delete.sh
@@ -3,4 +3,5 @@
 source $(dirname $(readlink -f $0))/script_begin.sh
 ## https://github.com/TurboLabIt/webstackup/blob/master/script/frameworks/wordpress/products-delete.sh
 source "${WEBSTACKUP_SCRIPT_DIR}frameworks/wordpress/products-delete.sh"
-source "${SCRIPT_DIR}cache-clear.sh"
+
+bash "${SCRIPT_DIR}cache-clear.sh"
