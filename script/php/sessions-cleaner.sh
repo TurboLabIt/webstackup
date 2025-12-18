76697cdd021a1fcb99051dca4a47419854f11a7f
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2025-12-18 10:46:59 +0100

    Magento cleaner

--- script/php/sessions-cleaner.sh
+++ script/php/sessions-cleaner.sh
@@ -25,7 +25,7 @@ fxInfo "Deleting files older than ##${PHP_SESSION_CLEANER_MAX_DAYS}## days from 
 if [ -d ${PHP_SESSIONS_DIR} ]; then
 
   fxInfo "There are ##$( ls -1 ${PHP_SESSIONS_DIR} | wc -l)## files before the cleanse"
-  sudo find ${PHP_SESSIONS_DIR} -type f -mmin +360 -exec rm {} \;
+  sudo find ${PHP_SESSIONS_DIR} -type f -mtime +${PHP_SESSION_CLEANER_MAX_DAYS} -exec rm {} \;
   fxOK "Cleanse completed ##$( ls -1 ${PHP_SESSIONS_DIR} | wc -l)## files remain"
 
 else
