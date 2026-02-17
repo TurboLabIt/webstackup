a9c2a64b6da37ffddf78b968e9d935c8a479e877
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2026-02-17 11:30:12 +0100

    WP_DEBUG_LOG the default debug filepath is public and web-facing (/wp-content/debug.log) => changing it to a private path

--- script/php-pages/wordpress/wp-config-extras.php
+++ script/php-pages/wordpress/wp-config-extras.php
@@ -13,3 +13,7 @@ define('DISABLE_WP_CRON', true);
 if( !defined( 'WP_POST_REVISIONS' ) ) {
     define('WP_POST_REVISIONS', false);
 }
+
+// the default debug filepath is public and web-facing (/wp-content/debug.log) => changing it to a private path
+// note: you must switch the logging on with `define('WP_DEBUG', false);` in wp-config
+define( 'WP_DEBUG_LOG', __DIR__ . '/../var/log/wordpress-debug.log' );
