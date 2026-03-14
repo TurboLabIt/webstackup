ff5e50d5d24733339fd3cb5249b20cfa496b15fd
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2026-03-14 15:46:42 +0100

    !!!MAJOR!!! Revamped WordPress setup

--- script/php-pages/wordpress/functions-extras.php
+++ script/php-pages/wordpress/functions-extras.php
@@ -1,1 +1,11 @@
 <?php
+const WSU_AUTOLOADER_FILE = __DIR__ . '/vendor/autoload.php';
+
+if( is_readable(WSU_AUTOLOADER_FILE) ) {
+    require_once WSU_AUTOLOADER_FILE;
+}
+
+// Initialize Timber.
+if ( class_exists( 'Timber\Timber' ) ) {
+    Timber\Timber::init();
+}
