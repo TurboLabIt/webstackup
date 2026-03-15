5fbd23bd2e4edd1713cc3fad1d808e8308c32039
Zane <info@turbolab.it>
2026-03-15 11:21:17 +0100

    add `define('WP_WSU_WEBPACK_ENABLED', false);` to wp-config to disable webstackup webpack functions

--- script/php-pages/wordpress/functions-extras.php
+++ script/php-pages/wordpress/functions-extras.php
@@ -63,56 +63,59 @@ if ( get_option( 'default_comment_status' ) != 'open' ) {
 
 
 //<editor-fold defaultstate="collapsed" desc="*** 📦 Webpack ***">
-/**
- * Utility to dynamically find and return the URL of a hashed Webpack asset.
- * @param string $directory The sub-directory inside build (e.g., 'js' or 'css').
- * @param string $prefix The base name of the file (e.g., 'main.min').
- * @param string $extension The file extension (e.g., 'js' or 'css').
- * @return string|bool      The file URL, or false if not found.
- */
-function get_webpack_hashed_asset($directory, $prefix, $extension)
-{
-    $theme_dir = get_template_directory();
-    $theme_uri = get_template_directory_uri();
+if(WP_WSU_WEBPACK_ENABLED) {
 
-    // Construct the absolute path pattern: /.../wp-content/themes/my-theme/assets/build/js/main.min.*.js
-    $pattern = sprintf('%s/assets/build/%s/%s.*.%s', $theme_dir, $directory, $prefix, $extension);
+    /**
+     * Utility to dynamically find and return the URL of a hashed Webpack asset.
+     * @param string $directory The sub-directory inside build (e.g., 'js' or 'css').
+     * @param string $prefix The base name of the file (e.g., 'main.min').
+     * @param string $extension The file extension (e.g., 'js' or 'css').
+     * @return string|bool      The file URL, or false if not found.
+     */
+    function get_webpack_hashed_asset($directory, $prefix, $extension)
+    {
+        $theme_dir = get_template_directory();
+        $theme_uri = get_template_directory_uri();
 
-    // glob() returns an array of files matching the pattern
-    $files = glob($pattern);
+        // Construct the absolute path pattern: /.../wp-content/themes/my-theme/assets/build/js/main.min.*.js
+        $pattern = sprintf('%s/assets/build/%s/%s.*.%s', $theme_dir, $directory, $prefix, $extension);
 
-    if (!empty($files) && is_array($files)) {
-        // Grab the first matched file
-        $absolute_path = $files[0];
+        // glob() returns an array of files matching the pattern
+        $files = glob($pattern);
 
-        // Swap the server directory path for the public URI
-        return str_replace($theme_dir, $theme_uri, $absolute_path);
+        if (!empty($files) && is_array($files)) {
+            // Grab the first matched file
+            $absolute_path = $files[0];
+
+            // Swap the server directory path for the public URI
+            return str_replace($theme_dir, $theme_uri, $absolute_path);
+        }
+
+        return false;
     }
 
-    return false;
+
+    /**
+     * Enqueue the theme's CSS and JS files.
+     */
+    function wsu_theme_enqueue_assets()
+    {
+        // 1. Enqueue the compiled SCSS -> CSS
+        $css_url = get_webpack_hashed_asset('css', 'main.min', 'css');
+        if ($css_url) {
+            // Notice the 4th parameter (version) is 'null'.
+            // We don't need WP's native ?ver= cache-busting because our filename has the hash!
+            wp_enqueue_style('wsu-webpack-theme-style', $css_url, array(), null);
+        }
+
+        // 2. Enqueue the compiled JS
+        $js_url = get_webpack_hashed_asset('js', 'main.min', 'js');
+        if ($js_url) {
+            // Load in footer (true) and declare jQuery as a dependency (since we excluded it in Webpack)
+            wp_enqueue_script('wsu-webpack-theme-script', $js_url, array('jquery'), null, true);
+        }
+    }
+
+    add_action('wp_enqueue_scripts', 'wsu_theme_enqueue_assets');
 }
-
-
-/**
- * Enqueue the theme's CSS and JS files.
- */
-function wsu_theme_enqueue_assets()
-{
-    // 1. Enqueue the compiled SCSS -> CSS
-    $css_url = get_webpack_hashed_asset('css', 'main.min', 'css');
-    if ($css_url) {
-        // Notice the 4th parameter (version) is 'null'.
-        // We don't need WP's native ?ver= cache-busting because our filename has the hash!
-        wp_enqueue_style('wsu-webpack-theme-style', $css_url, array(), null);
-    }
-
-    // 2. Enqueue the compiled JS
-    $js_url = get_webpack_hashed_asset('js', 'main.min', 'js');
-    if ($js_url) {
-        // Load in footer (true) and declare jQuery as a dependency (since we excluded it in Webpack)
-        wp_enqueue_script('wsu-webpack-theme-script', $js_url, array('jquery'), null, true);
-    }
-}
-
-add_action('wp_enqueue_scripts', 'wsu_theme_enqueue_assets');
 //</editor-fold>
--- script/php-pages/wordpress/wp-config-extras.php
+++ script/php-pages/wordpress/wp-config-extras.php
@@ -15,5 +15,10 @@ if( !defined( 'WP_POST_REVISIONS' ) ) {
 }
 
 // the default debug filepath is public and web-facing (/wp-content/debug.log) => changing it to a private path
-// note: you must switch the logging on with `define('WP_DEBUG', false);` in wp-config
+// note: you must switch the logging on with `define('WP_DEBUG', true);` in wp-config to activate the logging
 define( 'WP_DEBUG_LOG', __DIR__ . '/../var/log/wordpress-debug.log' );
+
+// Enable webstackup webpack functions - - add `define('WP_WSU_WEBPACK_ENABLED', false);` to wp-config to disable
+if( !defined( 'WP_WSU_WEBPACK_ENABLED' ) ) {
+    define('WP_WSU_WEBPACK_ENABLED', true);
+}
