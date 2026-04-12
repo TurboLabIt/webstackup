8d7e472525f94267ff5e3e6cac8f72ab95e6941d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2026-04-12 12:18:20 +0200

    Create github-cli-install.sh

---
+++ script/git/github-cli-install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+### AUTOMATIC GITHUB-CLI (gh) INSTALLER BY WEBSTACKUP
+# sudo apt install curl -y && curl -s https://raw.githubusercontent.com/TurboLabIt/webstackup/master/script/git/github-cli-install.sh | sudo bash
+#
+# Source: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
+
+## bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "💿 GitHub-CLI installer "
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
