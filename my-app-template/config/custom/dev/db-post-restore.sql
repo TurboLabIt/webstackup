d14246bbc801854b4997b9c4c0c7dfcef34a81d2
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2026-06-09 12:47:19 +0200

    db-post-restore: UPDATE `wp_blogs`

--- my-app-template/config/custom/dev/db-post-restore.sql
+++ my-app-template/config/custom/dev/db-post-restore.sql
@@ -10,6 +10,7 @@
 #USE my-app_dev0;
 #MAGENTO: UPDATE `core_config_data` SET `value` = 'https://dev0-next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
 #WORDPRESS: UPDATE `wp_options` SET `option_value` = 'https://dev0-next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
+#WORDPRESS: UPDATE `wp_blogs` SET `domain` = 'dev0-next.my-app.com' WHERE `blog_id` = 1;
 #WORDPRESS: UPDATE `wp_usermeta` SET `meta_value` = 'disabled' WHERE `meta_key` = 'googleauthenticator_enabled';
 
 
--- my-app-template/config/custom/staging/db-post-restore.sql
+++ my-app-template/config/custom/staging/db-post-restore.sql
@@ -7,5 +7,7 @@
 #
 # 🪄 Based on https://github.com/TurboLabIt/webstackup/blob/master/my-app-template/config/custom/staging/db-post-restore.sql
 
-#UPDATE `core_config_data` SET `value` = 'https://next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
-#UPDATE `wp_options` SET `option_value` = 'https://next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
+#MAGENTO: UPDATE `core_config_data` SET `value` = 'https://next.my-app.com/' WHERE `path` IN('web/unsecure/base_url', 'web/secure/base_url');
+#WORDPRESS: UPDATE `wp_options` SET `option_value` = 'https://next.my-app.com/' WHERE `option_name` IN('siteurl', 'home');
+#WORDPRESS: UPDATE `wp_blogs` SET `domain` = 'next.my-app.com' WHERE `blog_id` = 1;
+#WORDPRESS: UPDATE `wp_usermeta` SET `meta_value` = 'disabled' WHERE `meta_key` = 'googleauthenticator_enabled';
