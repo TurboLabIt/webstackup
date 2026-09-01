2cc1291b8495b8178c562bcc5f2ebe7876914157
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2026-09-01 17:52:32 +0200

    Create build.sh

---
+++ script/node.js/build.sh
@@ -0,0 +1,9 @@
+fxHeader "👀 ${APP_NAME} build"
+
+source "${WEBSTACKUP_SCRIPT_DIR}node.js/node_script_begin.sh"
+
+fxTitle "💿 npm install..."
+echo "y" | ${NPM_CMD} install
+
+fxTitle "🏗️ npm build..."
+echo "y" | ${NPM_CMD} run build
