7e2d4b08a1834d8bcb572af5498e491ece3b7736
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2026-09-03 11:44:57 +0200

    fix TypeError: dispatcher.getOwner is not a function in production

--- script/node.js/run.sh
+++ script/node.js/run.sh
@@ -3,4 +3,4 @@ fxHeader "🚀 ${APP_NAME} run"
 source "${WEBSTACKUP_SCRIPT_DIR}node.js/build.sh"
 
 fxTitle "🏃 Starting the server..."
-sudo -u $EXPECTED_USER -H PORT=$NODE_PORT node server.js build/server/index.js
+sudo -u $EXPECTED_USER -H PORT=$NODE_PORT NODE_ENV=$NODE_ENV node server.js build/server/index.js
